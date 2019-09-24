45dae0edb478ff4f60c2bf069f6dbec8c9fc2edf
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2019-09-24 12:25:37 +0200

    [WIP] Load a webview fragment with a specific issue

--- app/src/main/java/de/taz/app/android/MainActivity.kt
+++ app/src/main/java/de/taz/app/android/MainActivity.kt
@@ -3,19 +3,24 @@ package de.taz.app.android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.lifecycle.lifecycleScope
 import de.taz.app.android.api.ApiService
+import de.taz.app.android.api.models.Issue
+import de.taz.app.android.download.DownloadService
+import de.taz.app.android.fragments.WebViewFragment
 import de.taz.app.android.persistence.repository.IssueRepository
 import de.taz.app.android.util.AuthHelper
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.fragment_webview.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.File
 
-class MainActivity(private val apiService: ApiService = ApiService()) : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private val authHelper = AuthHelper.getInstance()
 
@@ -23,14 +28,12 @@ class MainActivity(private val apiService: ApiService = ApiService()) : AppCompa
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val apiService = ApiService()
+
         CoroutineScope(Dispatchers.IO).launch {
-            IssueRepository().getLatestIssue()?.let { lastIssue ->
-                val file = File(
-                    ContextCompat.getExternalFilesDirs(applicationContext, null).first(),
-                    "${lastIssue.tag}/${lastIssue.sectionList.first().sectionHtml.name}"
-                )
-                runOnUiThread { helloWorld.loadUrl("file://${file.absolutePath}") }
-            }
+            val lastIssue =  apiService.getIssueByFeedAndDate()
+            IssueRepository().save(lastIssue)
+            DownloadService.download(applicationContext, lastIssue)
         }
         login.setOnClickListener {
             GlobalScope.launch {
@@ -47,11 +50,11 @@ class MainActivity(private val apiService: ApiService = ApiService()) : AppCompa
         test.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 IssueRepository().getLatestIssue()?.let { lastIssue ->
-                    val file = File(
-                        ContextCompat.getExternalFilesDirs(applicationContext, null).first(),
-                        "${lastIssue.date}/${lastIssue.sectionList.first().sectionHtml.name}"
-                    )
-                    runOnUiThread { helloWorld.loadUrl("file://${file.absolutePath}") }
+
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentPlaceholder, WebViewFragment(lastIssue))
+                        .commit()
                 }
             }
         }
