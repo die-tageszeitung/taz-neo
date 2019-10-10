25a3b30d3379727b8cd8b20089f6cc4180119147
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2019-10-10 18:14:35 +0200

    Set section/article headers dynamically

--- app/src/main/java/de/taz/app/android/ui/webview/ArticleWebViewFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/webview/ArticleWebViewFragment.kt
@@ -1,5 +1,10 @@
 package de.taz.app.android.ui.webview
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import de.taz.app.android.R
@@ -14,16 +19,28 @@ class ArticleWebViewFragment(val article: Article) : WebViewFragment(), AppWebVi
     override val menuId: Int = R.menu.navigation_bottom_article
     override val headerId: Int = R.layout.fragment_webview_header_article
 
-    override fun onResume() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         CoroutineScope(Dispatchers.IO).launch {
-            val file = File(
-                ContextCompat.getExternalFilesDirs(
-                    requireActivity().applicationContext, null).first(),
-               "${article.getSection()!!.issueBase.tag}/${article.articleFileName}"
-            )
-            lifecycleScope.launch { fileLiveData.value = file}
+            article.getSection()?.let {section ->
+                val file = File(
+                    ContextCompat.getExternalFilesDirs(
+                        requireActivity().applicationContext, null
+                    ).first(),
+                    "${section.issueBase.tag}/${article.articleFileName}"
+                )
+                lifecycleScope.launch { fileLiveData.value = file }
+                activity?.runOnUiThread {
+                    view.findViewById<TextView>(R.id.section).apply {
+                        text = section.title
+                    }
+                    view.findViewById<TextView>(R.id.article_num).apply {
+                        text = "x/${section.articleList.size}"
+                    }
+                }
+            }
         }
-        super.onResume()
     }
 }
 
