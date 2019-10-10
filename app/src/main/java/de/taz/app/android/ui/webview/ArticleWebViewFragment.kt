fbeecaf63c9c966a6b9a1fb6bab3bd57771a878a
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2019-10-10 12:56:03 +0200

    Refactor all the things

--- app/src/main/java/de/taz/app/android/ui/webview/ArticleWebViewFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/webview/ArticleWebViewFragment.kt
@@ -3,6 +3,7 @@ package de.taz.app.android.ui.webview
 import android.os.Bundle
 import android.view.*
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import de.taz.app.android.R
 import de.taz.app.android.api.models.Article
 import kotlinx.coroutines.CoroutineScope
@@ -10,22 +11,23 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.File
 
-class ArticleWebViewFragment(val article: Article) : WebViewFragment(), ArticleWebViewCallback {
+class ArticleWebViewFragment(val article: Article) : WebViewFragment(), AppWebViewCallback {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_article_webview, container, false)
+        return inflater.inflate(R.layout.fragment_webview_article, container, false)
     }
 
     override fun onResume() {
         CoroutineScope(Dispatchers.IO).launch {
-            file = File(
+            val file = File(
                 ContextCompat.getExternalFilesDirs(
                     requireActivity().applicationContext, null).first(),
                "${article.getSection()!!.issueBase.tag}/${article.articleFileName}"
             )
+            lifecycleScope.launch { fileLiveData.value = file}
         }
         super.onResume()
     }
