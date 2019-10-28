2f5e1f8d0bfafe2b7ecc06f30719022095ac38bb
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2019-10-28 17:27:54 +0100

    Fix stuff found by tests

--- app/src/main/java/de/taz/app/android/persistence/dao/ArticleDao.kt
+++ app/src/main/java/de/taz/app/android/persistence/dao/ArticleDao.kt
@@ -8,12 +8,11 @@ import de.taz.app.android.api.models.ArticleBase
 @Dao
 abstract class ArticleDao : BaseDao<ArticleBase>() {
     @Query("SELECT * FROM Article WHERE Article.articleFileName == :articleFileName LIMIT 1")
-    abstract fun get(articleFileName: String): ArticleBase
+    abstract fun get(articleFileName: String): ArticleBase?
 
     @Query("SELECT * FROM Article WHERE Article.articleFileName in(:articleFileNames)")
     abstract fun get(articleFileNames: List<String>): List<ArticleBase>
 
     @Query("SELECT * FROM Article WHERE Article.bookmarked != 0")
     abstract fun getBookmarkedArticlesLiveData(): LiveData<List<ArticleBase>>
-
 }
