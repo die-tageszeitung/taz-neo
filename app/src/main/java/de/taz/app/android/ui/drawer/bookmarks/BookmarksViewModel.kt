83c48505d931c1413582c8203b0ed518ebce948b
eike <eike@ctrl.alt.coop>
2019-10-29 12:43:26 +0100

    Remove unused imports
    :fire: Imports where not used, so they can be removed.

--- app/src/main/java/de/taz/app/android/ui/drawer/bookmarks/BookmarksViewModel.kt
+++ app/src/main/java/de/taz/app/android/ui/drawer/bookmarks/BookmarksViewModel.kt
@@ -2,9 +2,7 @@ package de.taz.app.android.ui.drawer.bookmarks
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import de.taz.app.android.api.models.Article
 import de.taz.app.android.api.models.ArticleStub
-import de.taz.app.android.api.models.Issue
 
 class BookmarksViewModel : ViewModel() {
 
