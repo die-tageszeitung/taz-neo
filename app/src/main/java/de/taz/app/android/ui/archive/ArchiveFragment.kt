a8542657150f00959a3a37ec28369376fb1286f0
Felix Höffken <felix@ctrl.alt.coop>
2019-11-05 10:15:26 +0100

    Store bitmaps in DataController

--- app/src/main/java/de/taz/app/android/ui/archive/ArchiveFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/archive/ArchiveFragment.kt
@@ -1,21 +1,26 @@
 package de.taz.app.android.ui.archive
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
+import android.widget.ImageView
+import androidx.lifecycle.lifecycleScope
 import de.taz.app.android.R
 import de.taz.app.android.api.models.IssueStub
 import de.taz.app.android.base.BaseFragment
 import de.taz.app.android.ui.main.MainActivity
 import de.taz.app.android.ui.main.MainContract
 import kotlinx.android.synthetic.main.fragment_archive.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
 
 class ArchiveFragment : BaseFragment<ArchiveContract.Presenter>(), ArchiveContract.View {
 
     override val presenter = ArchivePresenter()
-    val archiveListAdapter= ArchiveListAdapter(this)
+    val archiveListAdapter = ArchiveListAdapter(this)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,13 +47,13 @@ class ArchiveFragment : BaseFragment<ArchiveContract.Presenter>(), ArchiveContra
         fragment_archive_grid.setOnItemClickListener { _, _, position, _ ->
             presenter.onItemSelected(archiveListAdapter.getItem(position))
         }
-        
+
         fragment_archive_grid.setOnScrollListener(ArchiveOnScrollListener(this))
 
     }
 
     override fun onDataSetChanged(issueStubs: List<IssueStub>) {
-        (fragment_archive_grid?.adapter as? ArchiveListAdapter)?.updateMomentList(issueStubs)
+        (fragment_archive_grid?.adapter as? ArchiveListAdapter)?.setIssueStubs(issueStubs)
     }
 
     override fun hideScrollView() {
@@ -59,4 +64,18 @@ class ArchiveFragment : BaseFragment<ArchiveContract.Presenter>(), ArchiveContra
         return activity as? MainActivity
     }
 
+    override fun addMoment(tag: String, bitmap: Bitmap) {
+        archiveListAdapter.addMomentBitmap(tag, bitmap)
+        lifecycleScope.launch(Dispatchers.Main) {
+            view?.findViewWithTag<View>(tag)?.apply {
+                findViewById<ImageView>(R.id.fragment_archive_moment_image).apply {
+                    setImageBitmap(bitmap)
+                    visibility = View.VISIBLE
+
+                }
+                findViewById<View>(R.id.fragment_archive_moment_image_progressbar).visibility =
+                    View.GONE
+            }
+        }
+    }
 }
--- app/src/main/java/de/taz/app/android/ui/archive/ArchiveListAdapter.kt
+++ app/src/main/java/de/taz/app/android/ui/archive/ArchiveListAdapter.kt
@@ -1,39 +1,25 @@
 package de.taz.app.android.ui.archive
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.GridView
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
 import de.taz.app.android.R
 import de.taz.app.android.api.models.Feed
 import de.taz.app.android.api.models.IssueStub
-import de.taz.app.android.api.models.Moment
-import de.taz.app.android.download.DownloadService
 import de.taz.app.android.persistence.repository.FeedRepository
-import de.taz.app.android.persistence.repository.MomentRepository
-import de.taz.app.android.util.FileHelper
-import de.taz.app.android.util.Log
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 
 class ArchiveListAdapter(private val archiveFragment: ArchiveFragment) : BaseAdapter() {
 
-    private val momentRepository = MomentRepository.getInstance(
-        archiveFragment.getMainView()?.getApplicationContext()
-    )
-
     private var issueStubList: List<IssueStub> = emptyList()
-    val issueMomentBitmapMap: MutableMap<String, Bitmap> = mutableMapOf()
+    private val issueMomentBitmapMap: MutableMap<String, Bitmap> = mutableMapOf()
+
     private val feeds: Map<String, Feed> = runBlocking(Dispatchers.IO) {
         FeedRepository.getInstance().getAll().associateBy { it.name }
     }
@@ -73,16 +59,6 @@ class ArchiveListAdapter(private val archiveFragment: ArchiveFragment) : BaseAda
             view.findViewById<View>(R.id.fragment_archive_moment_image_progressbar).visibility =
                 View.GONE
         } else {
-            archiveFragment.getLifecycleOwner().lifecycleScope.launch(Dispatchers.IO) {
-                momentRepository.get(issueStub)?.let { moment ->
-                    if (!moment.isDownloadedOrDownloading()) {
-                        archiveFragment.getMainView()?.getApplicationContext()?.let {
-                            DownloadService.download(it, moment)
-                        }
-                    }
-                }
-            }
-
             view.findViewById<ImageView>(R.id.fragment_archive_moment_image).visibility = View.GONE
             view.findViewById<View>(R.id.fragment_archive_moment_image_progressbar).visibility =
                 View.VISIBLE
@@ -100,82 +76,13 @@ class ArchiveListAdapter(private val archiveFragment: ArchiveFragment) : BaseAda
         }
     }
 
-    fun updateMomentList(issues: List<IssueStub>) {
+    fun addMomentBitmap(tag: String, bitmap: Bitmap) {
+        issueMomentBitmapMap[tag] = bitmap
+    }
+
+    fun setIssueStubs(issues: List<IssueStub>) {
         issueStubList = issues
-
-        issues.forEach { issueStub ->
-            if (!issueMomentBitmapMap.containsKey(issueStub.tag)) {
-                generateMomentBitmapForIssueStub(issueStub)
-            }
-        }
         notifyDataSetChanged()
-    }
-
-    private fun generateMomentBitmapForIssueStub(issueStub: IssueStub) {
-        archiveFragment.getLifecycleOwner().lifecycleScope.launch(Dispatchers.IO) {
-            momentRepository.get(issueStub)?.let { moment ->
-                val observer = ArchiveMomentDownloadObserver(
-                    archiveFragment,
-                    this@ArchiveListAdapter,
-                    issueStub,
-                    moment
-                )
-
-                withContext(Dispatchers.Main) {
-                    moment.isDownloadedLiveData().observe(
-                        archiveFragment.getLifecycleOwner(),
-                        observer
-                    )
-                }
-            }
-        }
-    }
-
-}
-
-class ArchiveMomentDownloadObserver(
-    private val archiveFragment: ArchiveFragment,
-    private val archiveListAdapter: ArchiveListAdapter,
-    private val issueStub: IssueStub,
-    private val moment: Moment
-) : Observer<Boolean> {
-
-    private val log by Log
-
-    private val fileHelper = FileHelper.getInstance(
-        archiveFragment.getMainView()?.getApplicationContext()
-    )
-
-    override fun onChanged(isDownloaded: Boolean?) {
-        archiveFragment.viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-            log.debug("issue: ${issueStub.tag} is downloaded? $isDownloaded")
-            if (isDownloaded == true) {
-                generateBitMapForMoment(issueStub, moment)
-                withContext(Dispatchers.Main) {
-                    moment.isDownloadedLiveData().removeObserver(this@ArchiveMomentDownloadObserver)
-                }
-            }
-        }
-    }
-
-    private suspend fun generateBitMapForMoment(issueStub: IssueStub, moment: Moment) {
-        moment.imageList.lastOrNull()?.let {
-            fileHelper.getFile("${issueStub.tag}/${it.name}").let { imgFile ->
-                if (imgFile.exists()) {
-                    archiveListAdapter.issueMomentBitmapMap[issueStub.tag] =
-                        BitmapFactory.decodeFile(imgFile.absolutePath)
-                    redrawViews()
-                }
-            }
-        }
-    }
-
-    private suspend fun redrawViews() {
-        // TODO improve performance…
-        withContext(Dispatchers.Main) {
-            archiveFragment.view?.findViewById<GridView>(R.id.fragment_archive_grid)
-                ?.invalidateViews()
-        }
     }
 
 }
