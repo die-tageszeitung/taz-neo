5b181e129c6a15cf16015f30d4dcfc9181cad575
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2019-11-11 15:58:06 +0100

    Implement previousArticle() and nextArticle()

--- app/src/main/java/de/taz/app/android/ui/webview/TazApiJS.kt
+++ app/src/main/java/de/taz/app/android/ui/webview/TazApiJS.kt
@@ -3,6 +3,7 @@ package de.taz.app.android.ui.webview
 import android.content.Context
 import android.webkit.JavascriptInterface
 import androidx.lifecycle.lifecycleScope
+import de.taz.app.android.R
 import de.taz.app.android.api.models.Article
 import de.taz.app.android.persistence.repository.ArticleRepository
 import de.taz.app.android.util.Log
@@ -50,11 +51,25 @@ class TazApiJS (val view: WebViewContract.View) {
     @JavascriptInterface
     fun nextArticle(position: Int = 0) {
         log.debug("nextArticle $position")
+        view.getMainView()?.let { mainView ->
+            mainView.getLifecycleOwner().lifecycleScope.launch(Dispatchers.IO) {
+                view.getWebViewDisplayable()?.next()?.let { next ->
+                    mainView.showInWebView(next, R.anim.slide_in_left, R.anim.slide_out_left)
+                }
+            }
+        }
     }
 
     @JavascriptInterface
     fun previousArticle(position: Int = 0) {
         log.debug("previousArticle $position")
+        view.getMainView()?.let { mainView ->
+            mainView.getLifecycleOwner().lifecycleScope.launch(Dispatchers.IO) {
+                view.getWebViewDisplayable()?.previous()?.let { previous ->
+                    mainView.showInWebView(previous, R.anim.slide_in_right, R.anim.slide_out_right)
+                }
+            }
+        }
     }
 
     @JavascriptInterface
