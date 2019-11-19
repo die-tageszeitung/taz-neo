f287b9b62b93ff2f4fd2a49ae781f77b8a140ae3
Jonatan Zint <jonatan@ctrl.alt.coop>
2019-11-19 22:48:42 +0100

    #33 added a rudimentary test, further testing seems impossible due to limited mocking capabilities

--- app/src/main/java/de/taz/app/android/api/models/Moment.kt
+++ app/src/main/java/de/taz/app/android/api/models/Moment.kt
@@ -4,7 +4,6 @@ import de.taz.app.android.api.dto.MomentDto
 import de.taz.app.android.api.interfaces.CacheableDownload
 import de.taz.app.android.api.interfaces.IssueOperations
 import de.taz.app.android.persistence.repository.IssueRepository
-import de.taz.app.android.persistence.repository.SectionRepository
 
 data class Moment(
     val imageList: List<FileEntry> = emptyList()
--- app/src/test/java/de/taz/app/android/ui/archive/item/ArchiveItemPresenterTest.kt
+++ app/src/test/java/de/taz/app/android/ui/archive/item/ArchiveItemPresenterTest.kt
@@ -1,7 +1,43 @@
 package de.taz.app.android.ui.archive.item
 
-import org.junit.Assert.*
+import com.nhaarman.mockitokotlin2.*
+import de.taz.app.android.TestLifecycleOwner
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
+@kotlinx.coroutines.ExperimentalCoroutinesApi
 class ArchiveItemPresenterTest {
 
+
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @Mock
+    lateinit var archiveItemContractView: ArchiveItemContract.View
+
+    private val lifecycleOwner = TestLifecycleOwner()
+
+    private lateinit var presenter: ArchiveItemPresenter
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+        MockitoAnnotations.initMocks(this)
+
+        doReturn(lifecycleOwner).`when`(archiveItemContractView).getLifecycleOwner()
+        presenter = ArchiveItemPresenter()
+
+        presenter.attach(archiveItemContractView)
+        presenter.onViewCreated(null)
+    }
+
+    @Test
+    fun clearIssue() {
+        presenter.clearIssue()
+        verify(archiveItemContractView, times(1)).clearIssue()
+    }
 }
