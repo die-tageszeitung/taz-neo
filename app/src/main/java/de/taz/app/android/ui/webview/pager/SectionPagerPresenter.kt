45514f8eb3a118de1c84aee4bd9cad8571ba82de
Johannes Dillmann <johannes@ctrl.alt.coop>
2019-11-20 16:43:27 +0100

    Add section pager

---
+++ app/src/main/java/de/taz/app/android/ui/webview/pager/SectionPagerPresenter.kt
@@ -0,0 +1,39 @@
+package de.taz.app.android.ui.webview.pager
+
+import android.os.Bundle
+import androidx.lifecycle.observe
+import de.taz.app.android.api.models.Article
+import de.taz.app.android.api.models.Section
+import de.taz.app.android.base.BasePresenter
+
+class SectionPagerPresenter : BasePresenter<SectionPagerContract.View, SectionPagerDataController>(
+    SectionPagerDataController::class.java
+), SectionPagerContract.Presenter {
+
+    override fun onViewCreated(savedInstanceState: Bundle?) {
+        val localView = getView()
+        val localViewModel = viewModel
+        if (localViewModel != null && localView != null) {
+            localViewModel.getSectionList().observe(localView.getLifecycleOwner()) { sections ->
+                if (sections.isNotEmpty()) {
+                    localView.setSections(sections, localViewModel.currentPosition)
+                }
+            }
+
+        }
+    }
+
+    override fun setInitialSection(section: Section) {
+        viewModel?.setInitialSection(section)
+    }
+
+    override fun setCurrrentPosition(position: Int) {
+        viewModel?.currentPosition = position
+    }
+
+    override fun onBackPressed() {
+        getView()?.getMainView()?.apply {
+            showArchive()
+        }
+    }
+}
