35a8037455a44b8820a2e389a96f6e0585cb744f
Felix Höffken <felix@ctrl.alt.coop>
2019-11-25 17:45:53 +0100

    Store data

--- app/src/main/java/de/taz/app/android/ui/settings/TextSizePreference.kt
+++ app/src/main/java/de/taz/app/android/ui/settings/TextSizePreference.kt
@@ -3,14 +3,13 @@ package de.taz.app.android.ui.settings
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.TextView
-import androidx.preference.Preference
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceViewHolder
 import de.taz.app.android.R
-import de.taz.app.android.util.ToastHelper
 
 class TextSizePreference @JvmOverloads constructor(
     context: Context, attributeSet: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0
-) : Preference (context, attributeSet, defStyleAttr, defStyleRes) {
+) : EditTextPreference (context, attributeSet, defStyleAttr, defStyleRes) {
 
     init {
         widgetLayoutResource = R.layout.custom_preference_font_size
@@ -21,11 +20,20 @@ class TextSizePreference @JvmOverloads constructor(
         holder?.let {
             it.itemView.isClickable = false
             it.findViewById(R.id.settings_text_decrease)?.setOnClickListener{
-                ToastHelper.getInstance().makeToast("descrease!")
+                modifySize(-10)
             }
-            it.findViewById(R.id.settings_text_increase)?.setOnClickListener{
-                ToastHelper.getInstance().makeToast("increase!")
+
+            val textView = it.findViewById(R.id.settings_text_size) as? TextView
+            textView?.text = getPersistedString("100")
+
+            it.findViewById(R.id.settings_text_increase)?.setOnClickListener {
+                modifySize(10)
             }
         }
     }
+
+    private fun modifySize(percentage: Int) {
+        text = (getPersistedString("100").toInt() + percentage).toString()
+    }
+
 }
