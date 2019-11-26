7a18a4c44a1e5d55bbcd7f9c217427f1934297b8
Jonatan Zint <jonatan@ctrl.alt.coop>
2019-11-26 10:12:55 +0100

    Removing Licence string as code is not from the AOSP but by me - fixing unused variable and id string

--- app/src/main/java/de/taz/app/android/ui/feed/VerticalViewPager.kt
+++ app/src/main/java/de/taz/app/android/ui/feed/VerticalViewPager.kt
@@ -5,31 +5,11 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.viewpager.widget.ViewPager
-import de.taz.app.android.util.Log
 
-
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-class VerticalViewPager @JvmOverloads constructor(
-    context: Context?,
+open class VerticalViewPager @JvmOverloads constructor(
+    context: Context,
     attrs: AttributeSet? = null
-) : ViewPager(context!!, attrs) {
-
-    val log by Log
+) : ViewPager(context, attrs) {
 
     init {
         setPageTransformer(true, VerticalPageTransformer())
