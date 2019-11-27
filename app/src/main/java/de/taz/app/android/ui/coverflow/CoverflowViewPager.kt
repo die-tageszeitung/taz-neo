fb3ee2662edb0854174093ce11f88dc09aa37fb2
Jonatan Zint <jonatan@ctrl.alt.coop>
2019-11-27 19:24:28 +0100

    Fix motion event conflicts
    Achieved by disabling the SwipeRefreshLayout if ViewPager shows Archive
    and disabling nested scrolling of recyclerview
    
    closes #57

--- app/src/main/java/de/taz/app/android/ui/coverflow/CoverflowViewPager.kt
+++ app/src/main/java/de/taz/app/android/ui/coverflow/CoverflowViewPager.kt
@@ -3,6 +3,7 @@ package de.taz.app.android.ui.coverflow
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
+import android.view.View
 import androidx.viewpager.widget.ViewPager
 import de.taz.app.android.util.Log
 
@@ -14,6 +15,7 @@ class CoverflowViewPager @JvmOverloads constructor(
 
     init {
         setPageTransformer(true, ZoomPageTransformer())
+        overScrollMode = View.OVER_SCROLL_NEVER
         offscreenPageLimit = 3
     }
 }
