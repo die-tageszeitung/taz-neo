2d676f1fb5395cf406fffab2856f75bbcc4fa667
Felix Höffken <felix@ctrl.alt.coop>
2019-12-04 11:29:07 +0100

    Rebase

--- app/src/main/java/de/taz/app/android/ui/home/page/coverflow/CoverflowFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/home/page/coverflow/CoverflowFragment.kt
@@ -5,19 +5,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.UiThread
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2
 import de.taz.app.android.R
 import de.taz.app.android.api.models.Feed
 import de.taz.app.android.api.models.IssueStub
 import de.taz.app.android.base.BaseMainFragment
+import de.taz.app.android.persistence.repository.IssueRepository
 import de.taz.app.android.ui.home.page.HomePageListAdapter
 import de.taz.app.android.ui.main.MainActivity
 import de.taz.app.android.ui.main.MainContract
+import de.taz.app.android.ui.main.MainDataController
 import de.taz.app.android.util.Log
 import kotlinx.android.synthetic.main.fragment_coverflow.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 
@@ -26,6 +31,7 @@ class CoverflowFragment : BaseMainFragment<CoverflowContract.Presenter>(), Cover
     override val presenter = CoverflowPresenter()
 
     val log by Log
+    private val issueRepository = IssueRepository.getInstance()
 
     private val coverFlowPagerAdapter = HomePageListAdapter(
         this@CoverflowFragment,
@@ -49,7 +55,6 @@ class CoverflowFragment : BaseMainFragment<CoverflowContract.Presenter>(), Cover
             adapter = coverFlowPagerAdapter
             registerOnPageChangeCallback(CoverFlowOnPageChangeCallback())
         }
-
         presenter.attach(this)
         presenter.onViewCreated(savedInstanceState)
     }
@@ -81,7 +86,7 @@ class CoverflowFragment : BaseMainFragment<CoverflowContract.Presenter>(), Cover
     }
 
     override fun getMainView(): MainContract.View? {
-        return activity as MainActivity
+        return activity as? MainActivity
     }
 
     override fun skipToEnd() {
@@ -93,16 +98,31 @@ class CoverflowFragment : BaseMainFragment<CoverflowContract.Presenter>(), Cover
     inner class CoverFlowOnPageChangeCallback : ViewPager2.OnPageChangeCallback() {
 
         override fun onPageSelected(position: Int) {
-            coverFlowPagerAdapter.let { adapter ->
-                viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
 
-                    val visibleItemCount = 3
-                    val totalItemCount = adapter.itemCount
+            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+                informSideBarOfIssueChange(position)
 
-                    if (position > totalItemCount - 2 * visibleItemCount) {
-                        adapter.getItem(0)?.date?.let { requestDate ->
-                            presenter.getNextIssueMoments(requestDate)
-                        }
+                val visibleItemCount = 3
+                val totalItemCount = coverFlowPagerAdapter.itemCount
+
+                if (position > totalItemCount - 2 * visibleItemCount) {
+                    coverFlowPagerAdapter.getItem(0)?.date?.let { requestDate ->
+                        presenter.getNextIssueMoments(requestDate)
+                    }
+                }
+            }
+
+        }
+
+        @UiThread
+        private fun informSideBarOfIssueChange(position: Int) {
+            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+                val viewModel = ViewModelProviders.of(requireActivity()).get(
+                    MainDataController::class.java
+                )
+                coverFlowPagerAdapter.getItem(position)?.let { issueStub ->
+                    issueRepository.getIssue(issueStub).let { issue ->
+                        viewModel.setIssue(issue)
                     }
                 }
             }
