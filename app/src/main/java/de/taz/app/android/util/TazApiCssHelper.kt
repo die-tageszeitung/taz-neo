2ed0a3f55a164901adbcd23942616290af55e607
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2019-12-05 12:12:01 +0100

    Move common functionality to a TazApiCssHelper
    this includes
    * css string creation
    * computing actual font size (for the css string creation)

---
+++ app/src/main/java/de/taz/app/android/util/TazApiCssHelper.kt
@@ -0,0 +1,35 @@
+package de.taz.app.android.util
+
+import android.content.Context
+import android.content.SharedPreferences
+import de.taz.app.android.download.RESOURCE_FOLDER
+
+const val SETTINGS_TEXT_DEFAULT_FONT_SIZE = 18
+
+class TazApiCssHelper private constructor(applicationContext: Context) {
+
+    companion object : SingletonHolder<TazApiCssHelper, Context>(::TazApiCssHelper)
+
+    private val fileHelper = FileHelper.getInstance()
+
+    /**
+     * Computes an actual font size using the default font size and the display percentage
+     * entered by the user
+     */
+    private fun computeFontSize(percentage: String) : String {
+        val fontSize = percentage.toInt() * 0.01 * SETTINGS_TEXT_DEFAULT_FONT_SIZE
+        return fontSize.toString()
+    }
+
+    fun generateCssString(sharedPreferences: SharedPreferences) : String {
+        val nightModeCssFile = fileHelper.getFile("$RESOURCE_FOLDER/themeNight.css")
+        val nightModeCssString = if (sharedPreferences.getBoolean("text_night_mode", false)) "@import \"$nightModeCssFile\";" else ""
+        val fontSizePx = computeFontSize(sharedPreferences.getString("text_font_size", "100") ?: "100")
+        return """
+            $nightModeCssString
+            html, body {
+                font-size: ${fontSizePx}px;
+            }
+        """.trimIndent()
+    }
+}
