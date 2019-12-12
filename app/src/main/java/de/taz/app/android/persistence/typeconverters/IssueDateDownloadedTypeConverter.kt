d6419b25ba255090310c6c2ea5cce5be997386ac
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2019-12-12 16:16:31 +0100

    Handle date type conversion for DB
    1. Compare date to "" instead of null in DB, since date is saved as a string
    2. Save not only date, but also time
    3. Handle empty values

--- app/src/main/java/de/taz/app/android/persistence/typeconverters/IssueDateDownloadedTypeConverter.kt
+++ app/src/main/java/de/taz/app/android/persistence/typeconverters/IssueDateDownloadedTypeConverter.kt
@@ -6,15 +6,21 @@ import java.util.*
 
 
 class IssueDateDownloadTypeConverter {
-    private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+    private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
 
     @TypeConverter
-    fun toString(issueDateDownload: Date): String {
-        return simpleDateFormat.format(issueDateDownload)
+    fun toString(issueDateDownload: Date?): String {
+        issueDateDownload?.let {
+            return simpleDateFormat.format(issueDateDownload)
+        }
+        return ""
     }
 
     @TypeConverter
     fun toIssueDateDownload(value: String): Date? {
-        return simpleDateFormat.parse(value) ?: Date()
+        if (value == "") {
+            return null
+        }
+        return simpleDateFormat.parse(value) ?: null
     }
 }
