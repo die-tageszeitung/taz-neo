a2e0f0fce2ea3245f2066b4c5982c37d300450c2
Felix Höffken <felix@ctrl.alt.coop>
2019-12-21 17:19:38 +0100

    Maintain position

--- app/src/main/java/de/taz/app/android/ui/webview/pager/ArticlePagerContract.kt
+++ app/src/main/java/de/taz/app/android/ui/webview/pager/ArticlePagerContract.kt
@@ -1,5 +1,6 @@
 package de.taz.app.android.ui.webview.pager
 
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import de.taz.app.android.api.models.Article
 import de.taz.app.android.api.models.Section
@@ -8,16 +9,20 @@ import de.taz.app.android.base.BaseContract
 interface ArticlePagerContract: BaseContract {
     interface View: BaseContract.View {
         fun setArticles(articles: List<Article>, currentPosition: Int)
+        fun persistPosition(position: Int)
     }
 
     interface Presenter: BaseContract.Presenter {
         fun setInitialArticle(article: Article)
-        fun setCurrrentPosition(position: Int)
+        fun getCurrentPosition(): Int?
+        fun setCurrentPosition(position: Int)
         fun onBackPressed()
     }
 
     interface DataController: BaseContract.DataController {
-        var currentPosition: Int
+        fun getCurrentPosition(): Int
+        fun setCurrentPosition(position: Int)
+        fun observeCurrentPosition(viewLifecycleOwner: LifecycleOwner, block: (Int) -> Unit)
         fun setInitialArticle(article: Article)
         suspend fun getCurrentSection(): Section?
         fun getArticleList(): LiveData<List<Article>>
--- app/src/main/java/de/taz/app/android/ui/webview/pager/ArticlePagerFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/webview/pager/ArticlePagerFragment.kt
@@ -1,6 +1,5 @@
 package de.taz.app.android.ui.webview.pager
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -32,6 +31,8 @@ class ArticlePagerFragment : BaseMainFragment<ArticlePagerPresenter>(),
     private var stableIdProvider: StableIdProvider? = null
     private var articlePagerAdapter: ArticlePagerAdapter? = null
 
+    private var currentPosition: Int? = null
+
     companion object {
         fun createInstance(initialArticle: Article): ArticlePagerFragment {
             // FIXME: think about using the Bundle with a  id and getting the data from the viewmodel directly
@@ -39,14 +40,6 @@ class ArticlePagerFragment : BaseMainFragment<ArticlePagerPresenter>(),
             fragment.initialArticle = initialArticle
             return fragment
         }
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        stableIdProvider = ViewModelProviders.of(this).get(StableIdViewModel::class.java).also {
-            articlePagerAdapter = ArticlePagerAdapter(this, it)
-        }
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -62,12 +55,26 @@ class ArticlePagerFragment : BaseMainFragment<ArticlePagerPresenter>(),
         // Initialize the presenter and let it call this fragment to render the pager
         presenter.onViewCreated(savedInstanceState)
 
+        stableIdProvider = ViewModelProviders.of(this).get(StableIdViewModel::class.java).also {
+            articlePagerAdapter = ArticlePagerAdapter(this, it)
+        }
+
         setupViewPager()
+
+        currentPosition?.let {
+            presenter.setCurrentPosition(it)
+            webview_pager_viewpager.currentItem = it
+        }
+
+    }
+
+    override fun persistPosition(position: Int) {
+        currentPosition = position
     }
 
     override fun onDestroyView() {
-        super.onDestroyView()
         webview_pager_viewpager?.adapter = null
+        super.onDestroyView()
     }
 
     override fun onCreateView(
@@ -95,7 +102,6 @@ class ArticlePagerFragment : BaseMainFragment<ArticlePagerPresenter>(),
         } as? ArticleWebViewFragment
     }
 
-
     private fun setupViewPager() {
         webview_pager_viewpager?.apply {
             adapter = articlePagerAdapter
@@ -107,7 +113,7 @@ class ArticlePagerFragment : BaseMainFragment<ArticlePagerPresenter>(),
 
     private val pageChangeListener = object : ViewPager2.OnPageChangeCallback() {
         override fun onPageSelected(position: Int) {
-            this@ArticlePagerFragment.presenter.setCurrrentPosition(position)
+            this@ArticlePagerFragment.presenter.setCurrentPosition(position)
         }
     }
 
