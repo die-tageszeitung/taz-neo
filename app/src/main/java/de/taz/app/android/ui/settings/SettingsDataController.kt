fa8959a1bf325d65eba766fda23d43c4a785cc76
Felix Höffken <felix@ctrl.alt.coop>
2020-01-14 12:24:19 +0100

    Rewrite settings without androidx preferences

--- app/src/main/java/de/taz/app/android/ui/settings/SettingsDataController.kt
+++ app/src/main/java/de/taz/app/android/ui/settings/SettingsDataController.kt
@@ -1,7 +1,65 @@
 package de.taz.app.android.ui.settings
 
+import android.content.Context
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import de.taz.app.android.PREFERENCES_GENERAL
+import de.taz.app.android.PREFERENCES_TAZAPICSS
 import de.taz.app.android.base.BaseDataController
+import de.taz.app.android.util.KEEP_ISSUES_DOWNLOADED_DEFAULT
+import de.taz.app.android.util.SharedPreferenceBooleanLiveData
+import de.taz.app.android.util.SharedPreferenceStringLiveData
 
-class SettingsDataController: BaseDataController() {
+class SettingsDataController : BaseDataController(), SettingsContract.DataController {
 
+    private lateinit var textSizeLiveData: SharedPreferenceStringLiveData
+    private lateinit var nightModeLiveData: SharedPreferenceBooleanLiveData
+    private lateinit var storedIssueNumberLiveData: SharedPreferenceStringLiveData
+
+    override fun observeNightMode(lifecycleOwner: LifecycleOwner, block: (Boolean) -> Unit) {
+        nightModeLiveData.observe(lifecycleOwner, Observer(block))
+    }
+
+    override fun observeTextSize(lifecycleOwner: LifecycleOwner, block: (String) -> Unit) {
+        textSizeLiveData.observe(lifecycleOwner, Observer(block))
+    }
+
+    override fun observeStoredIssueNumber(lifecycleOwner: LifecycleOwner, block: (String) -> Unit) {
+        storedIssueNumberLiveData.observe(lifecycleOwner, Observer(block))
+    }
+
+    override fun setStoredIssueNumber(number: Int) {
+        storedIssueNumberLiveData.postValue(number.toString())
+    }
+
+    override fun setNightMode(activated: Boolean) {
+        nightModeLiveData.postValue(activated)
+    }
+
+    override fun setTextSizePercent(percent: String) {
+        textSizeLiveData.postValue(percent)
+    }
+
+    override fun getTextSizePercent(): String {
+        return textSizeLiveData.value ?: "100"
+    }
+
+    override fun initializeSettings(applicationContext: Context) {
+        applicationContext.getSharedPreferences(PREFERENCES_TAZAPICSS, Context.MODE_PRIVATE)?.let {
+
+            textSizeLiveData =
+                SharedPreferenceStringLiveData(it, "text_font_size", "100")
+            nightModeLiveData =
+                SharedPreferenceBooleanLiveData(it, "text_night_mode", false)
+        }
+
+        applicationContext.getSharedPreferences(PREFERENCES_GENERAL, Context.MODE_PRIVATE)?.let {
+            storedIssueNumberLiveData = SharedPreferenceStringLiveData(
+                it,
+                "general_keep_number_issues",
+                KEEP_ISSUES_DOWNLOADED_DEFAULT.toString()
+            )
+
+        }
+    }
 }
---
+++ app/src/main/java/de/taz/app/android/ui/settings/SettingsFragment.kt
@@ -0,0 +1,131 @@
+package de.taz.app.android.ui.settings
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
+import de.taz.app.android.R
+import de.taz.app.android.base.BaseMainFragment
+import de.taz.app.android.ui.login.LoginFragment
+import java.util.*
+
+class SettingsFragment : BaseMainFragment<SettingsContract.Presenter>(), SettingsContract.View {
+
+    override val presenter = SettingsPresenter()
+
+    private var storedIssueNumber: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.attach(this)
+
+        view.apply {
+
+            findViewById<TextView>(R.id.fragment_settings_header_title).text =
+                getString(R.string.settings_header).toLowerCase(
+                    Locale.GERMAN
+                )
+
+            findViewById<TextView>(R.id.fragment_settings_general_keep_issues).apply {
+                setOnClickListener {
+                    showKeepIssuesDialog()
+                }
+            }
+
+
+            findViewById<Button>(R.id.fragment_settings_account_manage_account)
+                .setOnClickListener {
+                    this@SettingsFragment.getMainView()?.showMainFragment(LoginFragment())
+                }
+
+            findViewById<View>(R.id.settings_text_decrease).setOnClickListener {
+                presenter.decreaseTextSize()
+            }
+            findViewById<View>(R.id.settings_text_increase).setOnClickListener {
+                presenter.increaseTextSize()
+            }
+            findViewById<View>(R.id.settings_text_size).setOnClickListener {
+                presenter.resetTextSize()
+            }
+
+            findViewById<View>(R.id.fragment_settings_night_mode).apply {
+                val switch = findViewById<Switch>(R.id.preference_switch_switch)
+
+                findViewById<TextView>(R.id.preference_switch_title).apply {
+                    setText(R.string.settings_text_night_mode)
+                    setOnClickListener { switch.toggle() }
+                }
+
+
+                switch.setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        presenter.enableNightMode()
+                    } else {
+                        presenter.disableNightMode()
+                    }
+                }
+            }
+        }
+
+        presenter.onViewCreated(savedInstanceState)
+    }
+
+    private fun showKeepIssuesDialog() {
+        context?.let {
+            val dialog = AlertDialog.Builder(ContextThemeWrapper(it, R.style.DialogTheme))
+                .setView(R.layout.dialog_settings_keep_number)
+                .setPositiveButton(android.R.string.ok) { dialog, _ ->
+                    val editText = (dialog as AlertDialog).findViewById<EditText>(
+                        R.id.dialog_settings_keep_number
+                    )
+                    editText.text.toString().toIntOrNull()?.let { number ->
+                        presenter.setStoredIssueNumber(number)
+                        dialog.hide()
+                    }
+                }
+                .setNegativeButton(android.R.string.cancel) { dialog, _ ->
+                    (dialog as AlertDialog).hide()
+                }
+                .create()
+
+            dialog.show()
+            storedIssueNumber?.let {
+                dialog?.findViewById<TextView>(
+                    R.id.dialog_settings_keep_number
+                )?.text = storedIssueNumber
+            }
+        }
+    }
+
+    override fun showStoredIssueNumber(number: String) {
+        storedIssueNumber = number
+        val text = getString(R.string.settings_general_keep_number_issues, number)
+        view?.findViewById<TextView>(R.id.fragment_settings_general_keep_issues)?.text = text
+    }
+
+    override fun showNightMode(nightMode: Boolean) {
+        view?.findViewById<Switch>(R.id.preference_switch_switch)?.isChecked = nightMode
+    }
+
+    override fun showTextSize(textSize: Int) {
+        view?.findViewById<TextView>(
+            R.id.settings_text_size
+        )?.text = getString(R.string.percentage, textSize)
+    }
+
+    override fun onBottomNavigationItemClicked(menuItem: MenuItem, activated: Boolean) {
+        presenter.onBottomNavigationItemClicked(menuItem)
+    }
+
+}
