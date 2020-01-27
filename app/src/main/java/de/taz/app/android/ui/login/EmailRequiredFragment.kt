ca97063e777783835129dbd5773e0371954eaca4
Felix Höffken <felix@ctrl.alt.coop>
2020-01-27 18:03:46 +0100

    Add layouts

---
+++ app/src/main/java/de/taz/app/android/ui/login/EmailRequiredFragment.kt
@@ -0,0 +1,21 @@
+package de.taz.app.android.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import de.taz.app.android.R
+
+class EmailRequiredFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return LayoutInflater.from(activity)
+            .inflate(R.layout.fragment_login_email_required, container, false)
+    }
+
+}
