ee426215dfb708777f18b3961b120ae0dd57e03c
Felix Höffken <felix@ctrl.alt.coop>
2020-02-11 17:15:47 +0100

    Replace deprecated ViewModelProviders

--- app/src/main/java/de/taz/app/android/monkey/Fragment.kt
+++ app/src/main/java/de/taz/app/android/monkey/Fragment.kt
@@ -2,12 +2,12 @@ package de.taz.app.android.monkey
 
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import de.taz.app.android.base.BaseViewModelFactory
 
 inline fun <reified T : ViewModel> Fragment.getViewModel(noinline creator: (() -> T)? = null): T {
     return if (creator == null)
-        ViewModelProviders.of(this).get(T::class.java)
+        ViewModelProvider(this).get(T::class.java)
     else
-        ViewModelProviders.of(this, BaseViewModelFactory(creator)).get(T::class.java)
+        ViewModelProvider(this, BaseViewModelFactory(creator)).get(T::class.java)
 }
