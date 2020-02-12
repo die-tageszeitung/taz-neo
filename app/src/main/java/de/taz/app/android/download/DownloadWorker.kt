f598d5b49faafb482e39713a196c1497e990bfc8
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2020-02-12 16:40:25 +0100

    Refactor DownloadService and Worker to make them testable

--- app/src/main/java/de/taz/app/android/download/DownloadWorker.kt
+++ app/src/main/java/de/taz/app/android/download/DownloadWorker.kt
@@ -31,7 +31,22 @@ import java.util.*
 /**
  * Helper Object used by [WorkManagerDownloadWorker] and [DownloadService] to download
  */
-class DownloadWorker(private val httpClient: OkHttpClient = okHttpClient) {
+
+class DownloadWorker(
+    private val httpClient: OkHttpClient,
+    private val downloadRepository: DownloadRepository,
+    private val fileEntryRepository: FileEntryRepository,
+    private val fileHelper: FileHelper,
+    private val workManager: WorkManager
+) {
+
+    constructor(appContext: Context) : this(
+        okHttpClient,
+        DownloadRepository.getInstance(appContext),
+        FileEntryRepository.getInstance(appContext),
+        FileHelper.createInstance(appContext),
+        WorkManager.getInstance(appContext)
+    )
 
     private val log by Log
 
@@ -42,19 +57,18 @@ class DownloadWorker(private val httpClient: OkHttpClient = okHttpClient) {
      * @param downloads - [Download]s to download
      */
     suspend fun startDownloads(
-        appContext: Context,
         fileEntries: List<FileEntry>? = null,
         fileNames: List<String>? = null,
         downloads: List<Download>? = null
     ) {
         fileNames?.forEach {
-            startDownload(appContext, it)
+            startDownload(it)
         }
         downloads?.forEach {
-            startDownload(appContext, it.file.name)
+            startDownload(it.file.name)
         }
         fileEntries?.forEach {
-            startDownload(appContext, it.name)
+            startDownload(it.name)
         }
     }
 
@@ -63,10 +77,7 @@ class DownloadWorker(private val httpClient: OkHttpClient = okHttpClient) {
      * @param fileName - [FileEntry.name] of [FileEntry] to download
      */
     @UiThread
-    suspend fun startDownload(appContext: Context, fileName: String) {
-        val downloadRepository = DownloadRepository.getInstance(appContext)
-        val fileEntryRepository = FileEntryRepository.getInstance(appContext)
-        val fileHelper = FileHelper.createInstance(appContext)
+    suspend fun startDownload(fileName: String) {
 
         fileEntryRepository.get(fileName)?.let { fileEntry ->
             downloadRepository.get(fileName)?.let { fromDB ->
@@ -118,7 +129,7 @@ class DownloadWorker(private val httpClient: OkHttpClient = okHttpClient) {
                 // cancel workmanager request if downloaded successfully
                 if (fromDB.status == DownloadStatus.done) {
                     fromDB.workerManagerId?.let {
-                        WorkManager.getInstance(appContext).cancelWorkById(it)
+                        workManager.cancelWorkById(it)
                         log.info("canceling WorkerManagerRequest for ${fromDB.file.name}")
                     }
                     fromDB.workerManagerId = null
@@ -144,7 +155,7 @@ class WorkManagerDownloadWorker(
         inputData.getString(DATA_DOWNLOAD_FILE_NAME)?.let { fileName ->
             async {
                 try {
-                    DownloadWorker().startDownload(appContext, fileName)
+                    DownloadWorker(appContext).startDownload(fileName)
                     log.debug("download of $fileName succeeded")
                     Result.success()
                 } catch (ioe: IOException) {
