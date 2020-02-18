5b7511de58b386c5b40488e851f96c3b4d6b7769
Felix Höffken <felix@ctrl.alt.coop>
2020-02-18 12:17:58 +0100

    Enable email sending from SubscriptionInactiveFragment

--- app/src/main/java/de/taz/app/android/ui/login/fragments/SubscriptionInactiveFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/login/fragments/SubscriptionInactiveFragment.kt
@@ -6,11 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import de.taz.app.android.R
+import kotlinx.android.synthetic.main.fragment_login_subscription_inactive.*
 
 class SubscriptionInactiveFragment : BaseFragment(R.layout.fragment_login_subscription_inactive) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        fragment_login_subscription_inactive_email.setOnClickListener {
+            writeEmail()
+        }
 
     }
 
--- app/src/main/java/de/taz/app/android/ui/login/fragments/SubscriptionTakenFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/login/fragments/SubscriptionTakenFragment.kt
@@ -1,10 +1,8 @@
 package de.taz.app.android.ui.login.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import de.taz.app.android.R
-import de.taz.app.android.SUBSCRIPTION_EMAIL_ADDRESS
 import de.taz.app.android.ui.login.LoginViewModelState
 import kotlinx.android.synthetic.main.fragment_login_subscription_taken.*
 
@@ -25,15 +23,5 @@ class SubscriptionTakenFragment: BaseFragment(R.layout.fragment_login_subscripti
             writeEmail()
         }
     }
-
-    private fun writeEmail(to: String = SUBSCRIPTION_EMAIL_ADDRESS) {
-        val email = Intent(Intent.ACTION_SEND)
-        email.putExtra(Intent.EXTRA_EMAIL, arrayOf(to))
-        email.putExtra(Intent.EXTRA_SUBJECT, "") // TODO
-        email.putExtra(Intent.EXTRA_TEXT, "") // TODO
-        email.type = "message/rfc822"
-        startActivity(Intent.createChooser(email, null))
-    }
-
 
 }
