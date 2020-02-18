764cc067695cc58e16a771ffddf7bc605faf6ed0
Felix Höffken <felix@ctrl.alt.coop>
2020-02-18 16:52:43 +0100

    Better handle MissincCredentialsFragment interactions

--- app/src/main/java/de/taz/app/android/ui/login/fragments/CredentialsMissingFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/login/fragments/CredentialsMissingFragment.kt
@@ -1,7 +1,10 @@
 package de.taz.app.android.ui.login.fragments
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import de.taz.app.android.R
 import kotlinx.android.synthetic.main.fragment_login_missing_credentials.*
 
@@ -11,15 +14,12 @@ class CredentialsMissingFragment : BaseFragment(R.layout.fragment_login_missing_
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.username?.let {
-           fragment_login_missing_credentials_email_text.setText(it)
+            fragment_login_missing_credentials_email_text.setText(it)
         }
 
 
         fragment_login_missing_credentials_login.setOnClickListener {
-            viewModel.connect(
-                fragment_login_missing_credentials_email_text.text.toString(),
-                fragment_login_missing_credentials_password_text.text.toString()
-            )
+            connect()
         }
 
         fragment_login_missing_credentials_forgot_password_button?.setOnClickListener {
@@ -32,6 +32,73 @@ class CredentialsMissingFragment : BaseFragment(R.layout.fragment_login_missing_
             val email = fragment_login_missing_credentials_email_text.toString()
             viewModel.requestPasswordReset(email)
         }
+
+        fragment_login_missing_credentials_surname.setOnEditorActionListener(
+            object : TextView.OnEditorActionListener {
+                override fun onEditorAction(
+                    v: TextView?,
+                    actionId: Int,
+                    event: KeyEvent?
+                ): Boolean {
+                    if (actionId == EditorInfo.IME_ACTION_DONE) {
+                        connect()
+                        return true
+                    }
+                    return false
+                }
+            }
+        )
+
+    }
+
+    private fun connect() {
+        val email = fragment_login_missing_credentials_email_text.text.toString()
+        val password = fragment_login_missing_credentials_password.text.toString()
+
+        val passwordConfirm =
+            fragment_login_missing_credentials_password_confirmation.text.toString()
+        val firstName = fragment_login_missing_credentials_first_name.text.toString()
+        val surName = fragment_login_missing_credentials_surname.text.toString()
+
+        if (passwordConfirm.isNotEmpty()) {
+            if (password != passwordConfirm) {
+                fragment_login_missing_credentials_password.error = getString(
+                    R.string.login_password_confirmation_error_match
+                )
+                return
+            }
+            if (firstName.isEmpty()) {
+                fragment_login_missing_credentials_first_name.error = getString(
+                    R.string.login_first_name_error_empty
+                )
+                return
+            }
+            if (surName.isEmpty()) {
+                fragment_login_missing_credentials_surname.error = getString(
+                    R.string.login_surname_error_empty
+                )
+                return
+            }
+        }
+
+        val isEmailEmpty = email.isEmpty()
+        val isPasswordEmpty = password.isEmpty()
+
+        if (isEmailEmpty || isPasswordEmpty) {
+            if (isEmailEmpty) {
+                fragment_login_missing_credentials_email_text.error = getString(
+                    R.string.login_email_error_empty
+                )
+            }
+            if (password.isEmpty()) {
+                fragment_login_missing_credentials_password.error = getString(
+                    R.string.login_password_error_empty
+                )
+            }
+            return
+        }
+
+        viewModel.connect(email, password)
     }
 
 }
