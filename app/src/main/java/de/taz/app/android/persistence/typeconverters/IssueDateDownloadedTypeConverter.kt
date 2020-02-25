218560f67b4b562d0022ad257ec4ae3ddefc362d
eike <eike@ctrl.alt.coop>
2020-02-25 17:25:28 +0100

    Add catch with Sentry.capture
    (no branch): (no branch):

--- app/src/main/java/de/taz/app/android/persistence/typeconverters/IssueDateDownloadedTypeConverter.kt
+++ app/src/main/java/de/taz/app/android/persistence/typeconverters/IssueDateDownloadedTypeConverter.kt
@@ -1,15 +1,23 @@
 package de.taz.app.android.persistence.typeconverters
 
+
 import androidx.room.TypeConverter
+import de.taz.app.android.util.Log
+import io.sentry.Sentry
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class IssueDateDownloadTypeConverter {
     private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
+    private val FORMAT_STRING = "HH:mm:ss.SSSZ"
+    private val log by Log
 
     @TypeConverter
     fun toString(issueDateDownload: Date?): String {
+        val simpleDateFormat = SimpleDateFormat(FORMAT_STRING, Locale.US)
+
         issueDateDownload?.let {
             return simpleDateFormat.format(issueDateDownload)
         }
@@ -18,9 +26,17 @@ class IssueDateDownloadTypeConverter {
 
     @TypeConverter
     fun toIssueDateDownload(value: String): Date? {
+        val simpleDateFormat = SimpleDateFormat(FORMAT_STRING, Locale.US)
+
         if (value == "") {
             return null
         }
-        return simpleDateFormat.parse(value) ?: null
+        return try {
+            simpleDateFormat.parse(value)
+        } catch (e: ParseException) {
+            log.error("SHIT HAPPENED: ${e.message}")
+            Sentry.capture(e)
+            return null
+        }
     }
 }
