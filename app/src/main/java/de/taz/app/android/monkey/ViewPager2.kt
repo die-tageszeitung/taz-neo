224478672267779acc79df5b425624f0a1f6200e
Felix Höffken <felix@ctrl.alt.coop>
2020-03-05 18:17:33 +0100

    Monkey patch ViewPager2 so it can be shown below statusbar

--- app/src/main/java/de/taz/app/android/monkey/ViewPager2.kt
+++ app/src/main/java/de/taz/app/android/monkey/ViewPager2.kt
@@ -1,5 +1,6 @@
 package de.taz.app.android.monkey
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 
@@ -17,4 +18,39 @@ fun ViewPager2.reduceDragSensitivity(factor: Int = 8) {
     touchSlopField.isAccessible = true
     val touchSlop = touchSlopField.get(recyclerView) as Int
     touchSlopField.set(recyclerView, touchSlop * factor)
+}
+
+/**
+ * Monkey patching ViewPager2 so that the recyclerview is shown beneath the status bar
+ */
+fun ViewPager2.moveContentBeneathStatusBar() {
+    val recyclerViewField = ViewPager2::class.java.getDeclaredField("mRecyclerView")
+    recyclerViewField.isAccessible = true
+    val recyclerView = recyclerViewField.get(this) as RecyclerView
+
+    setOnApplyWindowInsetsListener { v, insets ->
+        (v.layoutParams as ViewGroup.MarginLayoutParams).apply {
+            topMargin = 0
+            leftMargin = insets.systemWindowInsetLeft
+            rightMargin = insets.systemWindowInsetRight
+            bottomMargin = insets.systemWindowInsetBottom
+        }
+        // trigger for recyclerview as well
+        for (index in 0 until childCount) getChildAt(index).dispatchApplyWindowInsets(insets)
+        insets.consumeSystemWindowInsets()
+    }
+
+    recyclerView.setOnApplyWindowInsetsListener { v, insets ->
+        val layoutParams = ViewGroup.MarginLayoutParams(v.layoutParams)
+        layoutParams.apply {
+            topMargin = 0
+            leftMargin = insets.systemWindowInsetLeft
+            rightMargin = insets.systemWindowInsetRight
+            bottomMargin = insets.systemWindowInsetBottom
+        }
+        v.layoutParams = layoutParams
+        insets.consumeSystemWindowInsets()
+    }
+
+    requestApplyInsets()
 }
--- app/src/main/java/de/taz/app/android/ui/webview/pager/ArticlePagerFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/webview/pager/ArticlePagerFragment.kt
@@ -11,6 +11,7 @@ import androidx.viewpager2.widget.ViewPager2
 import de.taz.app.android.R
 import de.taz.app.android.api.models.Article
 import de.taz.app.android.base.BaseMainFragment
+import de.taz.app.android.monkey.moveContentBeneathStatusBar
 import de.taz.app.android.monkey.reduceDragSensitivity
 import de.taz.app.android.ui.BackFragment
 import de.taz.app.android.ui.webview.ArticleWebViewFragment
@@ -56,7 +57,10 @@ class ArticlePagerFragment : BaseMainFragment<ArticlePagerPresenter>(),
         // Ensure initial fragment states are copied to the model via the presenter
         initialArticle?.let { presenter.setInitialArticle(it, bookmarksArticle) }
 
-        webview_pager_viewpager.reduceDragSensitivity(2)
+        webview_pager_viewpager.apply {
+            reduceDragSensitivity(2)
+            moveContentBeneathStatusBar()
+        }
         // Initialize the presenter and let it call this fragment to render the pager
         presenter.onViewCreated(savedInstanceState)
 
