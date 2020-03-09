77fd88f7578549fc281578b9dc6d0bb9d4e8bfdf
Felix Höffken <felix@ctrl.alt.coop>
2020-03-09 18:03:09 +0100

    Add pushtoken to Queries

--- app/src/main/java/de/taz/app/android/api/variables/TrialSubscriptionVariables.kt
+++ app/src/main/java/de/taz/app/android/api/variables/TrialSubscriptionVariables.kt
@@ -1,6 +1,7 @@
 package de.taz.app.android.api.variables
 
 import com.squareup.moshi.JsonClass
+import de.taz.app.android.api.dto.DeviceType
 import de.taz.app.android.firebase.FirebaseHelper
 import de.taz.app.android.singletons.AuthHelper
 import de.taz.app.android.singletons.JsonHelper
@@ -13,7 +14,8 @@ data class TrialSubscriptionVariables(
     val surname: String? = null,
     val firstName: String? = null,
     val installationId: String = AuthHelper.getInstance().installationId,
-    val deviceId: String? = FirebaseHelper.getInstance().firebaseToken
+    val pushToken: String? = FirebaseHelper.getInstance().firebaseToken,
+    val deviceType: DeviceType = DeviceType.android
 ) : Variables {
     override fun toJson() = JsonHelper.toJson(this)
 }
