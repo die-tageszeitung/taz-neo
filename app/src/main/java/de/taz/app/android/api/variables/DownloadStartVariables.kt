ebb519aa49c0303c625ac2025def5fd0fea728d8
Felix Höffken <felix@ctrl.alt.coop>
2020-03-10 10:16:41 +0100

    Remove logs and use parent's class log

--- app/src/main/java/de/taz/app/android/api/variables/DownloadStartVariables.kt
+++ app/src/main/java/de/taz/app/android/api/variables/DownloadStartVariables.kt
@@ -1,7 +1,6 @@
 package de.taz.app.android.api.variables
 
 import com.squareup.moshi.JsonClass
-import com.squareup.moshi.Moshi
 import de.taz.app.android.BuildConfig
 import de.taz.app.android.api.dto.DeviceFormat
 import de.taz.app.android.api.dto.DeviceType
