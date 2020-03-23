98f96da62582b87a2adde7d93265d892ffcf915c
Felix Höffken <felix@ctrl.alt.coop>
2020-03-23 18:49:28 +0100

    Fix tests

--- app/src/androidTest/java/de/taz/app/android/api/models/IssueTest.kt
+++ app/src/androidTest/java/de/taz/app/android/api/models/IssueTest.kt
@@ -1,6 +1,7 @@
 package de.taz.app.android.api.models
 
 import de.taz.app.android.IssueTestUtil
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,7 +11,7 @@ class IssueTest {
     private val issue = IssueTestUtil.getIssue()
 
     @Test
-    fun getAllFiles() {
+    fun getAllFiles() = runBlocking {
         val fileList = issue.getAllFiles()
 
         issue.moment.getMomentImage().let{ fileEntry ->
