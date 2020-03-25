b79ff7940f76625efb455575224a6997252eed7d
Felix Höffken <felix@ctrl.alt.coop>
2020-03-25 13:34:10 +0100

    Add SectionNavButtonJoin

---
+++ app/src/main/java/de/taz/app/android/persistence/join/SectionNavButtonJoin.kt
@@ -0,0 +1,30 @@
+package de.taz.app.android.persistence.join
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import de.taz.app.android.api.models.Image
+import de.taz.app.android.api.models.SectionStub
+
+@Entity(
+    tableName = "SectionNavButtonJoin",
+    foreignKeys = [
+        ForeignKey(
+            entity = SectionStub::class,
+            parentColumns = ["sectionFileName"],
+            childColumns = ["sectionFileName"]
+        ),
+        ForeignKey(
+            entity = Image::class,
+            parentColumns = ["name"],
+            childColumns = ["navButtonFileName"]
+        )
+    ],
+    primaryKeys = ["sectionFileName", "navButtonFileName"],
+    indices = [Index("navButtonFileName")]
+)
+data class SectionNavButtonJoin(
+    val sectionFileName: String,
+    val navButtonFileName: String,
+    val index: Int
+)
