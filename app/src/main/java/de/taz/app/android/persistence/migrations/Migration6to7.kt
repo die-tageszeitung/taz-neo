f1ff80ef60a3c19759cb788955eedaf32c1a58fb
Felix Höffken <felix@ctrl.alt.coop>
2020-03-27 15:50:47 +0100

    Correct migration

--- app/src/main/java/de/taz/app/android/persistence/migrations/Migration6to7.kt
+++ app/src/main/java/de/taz/app/android/persistence/migrations/Migration6to7.kt
@@ -6,7 +6,13 @@ import de.taz.app.android.persistence.repository.IssueRepository
 
 object Migration6to7 : Migration(6, 7) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("""
+        database.execSQL("CREATE TABLE Image (`name` TEXT NOT NULL, `storageType` TEXT NOT NULL, `moTime` INTEGER NOT NULL, `sha256` TEXT NOT NULL, `size` INTEGER NOT NULL, `folder` TEXT NOT NULL, `type` TEXT NOT NULL, `alpha` REAL NOT NULL, `resolution` TEXT NOT NULL, PRIMARY KEY(`name`, `storageType`))")
+
+        database.execSQL("CREATE TABLE SectionNavButtonJoin  (`sectionFileName` TEXT NOT NULL, `navButtonFileName` TEXT NOT NULL, `navButtonStorageType` TEXT NOT NULL, PRIMARY KEY(`sectionFileName`, `navButtonFileName`, `navButtonStorageType`), FOREIGN KEY(`sectionFileName`) REFERENCES `Section`(`sectionFileName`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`navButtonFileName`, `navButtonStorageType`) REFERENCES `Image`(`name`, `storageType`) ON UPDATE NO ACTION ON DELETE NO ACTION )")
+        database.execSQL("CREATE INDEX IF NOT EXISTS `index_SectionNavButtonJoin_sectionFileName` ON SectionNavButtonJoin (`sectionFileName`)")
+
+        database.execSQL(
+            """
             INSERT INTO Image (
                 name, storageType, moTime, sha256, size, folder, type, alpha, resolution
             ) VALUES (
@@ -16,9 +22,11 @@ object Migration6to7 : Migration(6, 7) {
             )""".trimMargin()
         )
 
-        database.execSQL("""
-            Insert SectionNavButtonJoin(sectionFileName, navButtonFileName, navButtonStorageType)
-            Select sectionFileName, 'navButton.taz.normal.png', 'resource' 
+        database.execSQL(
+            """
+            INSERT INTO SectionNavButtonJoin(
+                sectionFileName, navButtonFileName, navButtonStorageType
+            ) SELECT sectionFileName, 'navButton.taz.normal.png', 'resource'
             From Section 
             """.trimIndent()
         )
