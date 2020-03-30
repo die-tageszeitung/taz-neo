44746b4249a1a1c161c38eb01329830d4f509835
Felix Höffken <felix@ctrl.alt.coop>
2020-03-30 12:01:59 +0200

    Reduce fade in time

--- app/src/main/java/de/taz/app/android/monkey/ImageView.kt
+++ app/src/main/java/de/taz/app/android/monkey/ImageView.kt
@@ -7,7 +7,7 @@ fun ImageView.animatedChange(newImage: Bitmap, targetAlpha: Float= 1f) {
     animate().alpha(0f).setDuration(100L).withEndAction {
         setImageBitmap(newImage)
         imageAlpha = (targetAlpha * 255).toInt()
-        animate().alpha(1f).duration = 400
+        animate().alpha(1f).duration = 100
     }
 
 }
