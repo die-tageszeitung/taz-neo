21468aae7df36297745be80fbc286d391e36db3c
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2020-04-15 16:02:18 +0200

    Remove obsolete files

--- app/src/main/java/de/taz/app/android/ui/bottomSheet/datePicker/DatePickerDataController.kt
+++
@@ -1,7 +0,0 @@
-package de.taz.app.android.ui.bottomSheet.datePicker
-
-import de.taz.app.android.base.BaseDataController
-
-class DatePickerDataController :  BaseDataController(), DatePickerContract.DataController {
-
-}
