707d0a9ddf14f192b2390117278b181893d7a28b
Felix Höffken <felix@ctrl.alt.coop>
2020-04-27 14:37:23 +0200

    Migrate SectionNavButtonJoin

--- app/src/main/java/de/taz/app/android/persistence/migrations/Migration7to8.kt
+++ app/src/main/java/de/taz/app/android/persistence/migrations/Migration7to8.kt
@@ -6,7 +6,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 object Migration7to8 : Migration(7, 8) {
     override fun migrate(database: SupportSQLiteDatabase) {
 
-        database.execSQL("INSERT INTO FileEntry SELECT name, storageType, moTime sha256, size, folder FROM Image")
+        database.execSQL("INSERT INTO FileEntry SELECT name, storageType, moTime, sha256, size, folder FROM Image")
 
         database.execSQL("CREATE TABLE ImageStub (`fileEntryName` TEXT NOT NULL, `type` TEXT NOT NULL, `alpha` REAL NOT NULL, `resolution` TEXT NOT NULL, PRIMARY KEY(`fileEntryName`))")
         database.execSQL("INSERT INTO ImageStub (fileEntryName, type, alpha, resolution) SELECT name, type, alpha, resolution FROM Image")
@@ -15,6 +15,11 @@ object Migration7to8 : Migration(7, 8) {
 
         database.execSQL("ALTER TABLE ImageStub RENAME TO Image")
 
+        database.execSQL("CREATE TABLE SNBJ2 (`sectionFileName` TEXT NOT NULL, `navButtonFileName` TEXT NOT NULL, PRIMARY KEY(`sectionFileName`, `navButtonFileName`), FOREIGN KEY(`sectionFileName`) REFERENCES `Section`(`sectionFileName`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`navButtonFileName`) REFERENCES `Image`(`fileEntryName`) ON UPDATE NO ACTION ON DELETE NO ACTION )")
+        database.execSQL("INSERT INTO SNBJ2 (sectionFileName, navButtonFileName) SELECT sectionFileName, navButtonFileName FROM SectionNavButtonJoin")
+        database.execSQL("DROP TABLE SectionNavButtonJoin")
+        database.execSQL("ALTER TABLE SNBJ2 RENAME TO SectionNavButtonJoin")
+        database.execSQL("CREATE INDEX IF NOT EXISTS `index_SectionNavButtonJoin_sectionFileName` ON SectionNavButtonJoin(`sectionFileName`)")
 
         database.execSQL(
             """
