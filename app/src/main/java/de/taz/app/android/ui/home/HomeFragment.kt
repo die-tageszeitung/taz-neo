a5b1cf18434e4b640671e2c44d9dcd27c6e0b251
Felix Höffken <felix@ctrl.alt.coop>
2020-04-28 09:24:58 +0200

    Remove left over debug call

--- app/src/main/java/de/taz/app/android/ui/home/HomeFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/home/HomeFragment.kt
@@ -62,7 +62,6 @@ class HomeFragment : ViewModelBaseMainFragment(R.layout.fragment_feed) {
     }
 
     private suspend fun onRefresh() {
-        log.debug("onRefresh")
         withContext(Dispatchers.IO) {
             try {
                 val apiService = ApiService.getInstance(activity?.applicationContext)
