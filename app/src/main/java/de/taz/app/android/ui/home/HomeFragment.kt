ae032cef4ab4a83b9a3d743f02ac097d919c669f
Felix Höffken <felix@ctrl.alt.coop>
2020-04-28 07:06:33 +0200

    Refresh on swipe if at end

--- app/src/main/java/de/taz/app/android/ui/home/HomeFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/home/HomeFragment.kt
@@ -62,6 +62,7 @@ class HomeFragment : ViewModelBaseMainFragment(R.layout.fragment_home) {
     }
 
     private suspend fun onRefresh() {
+        log.debug("onRefresh")
         withContext(Dispatchers.IO) {
             try {
                 val apiService = ApiService.getInstance(activity?.applicationContext)
