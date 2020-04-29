127bcacf7c04323e309b67416788e4e48136f01b
Felix Höffken <felix@ctrl.alt.coop>
2020-04-29 12:06:35 +0200

    Recreate image information after delete

--- app/src/main/java/de/taz/app/android/persistence/dao/FileEntryDao.kt
+++ app/src/main/java/de/taz/app/android/persistence/dao/FileEntryDao.kt
@@ -10,7 +10,13 @@ abstract class FileEntryDao: BaseDao<FileEntry>() {
     @Query("SELECT * FROM FileEntry WHERE name == :name")
     abstract fun getByName(name: String): FileEntry?
 
-    @Query("SELECT * FROM FileEntry WHERE name in (:names)")
+    @Query("SELECT * FROM FileEntry WHERE name IN (:names)")
     abstract fun getByNames(names: List<String>): List<FileEntry>
+
+    @Query("SELECT * FROM FileEntry WHERE name LIKE :filterString")
+    abstract fun filterByName(filterString: String): List<FileEntry>
+
+   @Query("SELECT FileEntry.name FROM FileEntry WHERE name LIKE :filterString")
+    abstract fun getNamesContaining(filterString: String): List<String>
 
 }
