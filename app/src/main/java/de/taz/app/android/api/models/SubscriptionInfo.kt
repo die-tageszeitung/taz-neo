d91a29dce83aa997edbf45a5b9a3a4e1e43546d0
Felix Höffken <felix@ctrl.alt.coop>
2020-05-05 13:38:39 +0200

    Correctly anotate enums for moshi

--- app/src/main/java/de/taz/app/android/api/models/SubscriptionInfo.kt
+++ app/src/main/java/de/taz/app/android/api/models/SubscriptionInfo.kt
@@ -1,5 +1,8 @@
 package de.taz.app.android.api.models
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class SubscriptionInfo(
     val status: SubscriptionStatus,
     val message: String? = null,
