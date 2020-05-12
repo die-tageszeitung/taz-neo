8562c23358064a35beb44514aaa5fb49e433d802
Felix Höffken <felix@ctrl.alt.coop>
2020-05-12 13:39:56 +0200

    Set NO_ANIMATION flag

--- app/src/main/java/de/taz/app/android/ui/splash/SplashActivity.kt
+++ app/src/main/java/de/taz/app/android/ui/splash/SplashActivity.kt
@@ -65,12 +65,18 @@ class SplashActivity : AppCompatActivity() {
 
         if (isDataPolicyAccepted()) {
             if (isFirstTimeStart()) {
-                startActivity(Intent(this, WelcomeActivity::class.java))
+                val intent = Intent(this, WelcomeActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION or Intent.FLAG_ACTIVITY_NO_HISTORY)
+                startActivity(intent)
             } else {
-                startActivity(Intent(this, MainActivity::class.java))
+                val intent = Intent(this, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
             }
         } else {
-            startActivity(Intent(this, DataPolicyActivity::class.java))
+            val intent =Intent(this, DataPolicyActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION or Intent.FLAG_ACTIVITY_NO_HISTORY)
+            startActivity(intent)
         }
     }
 
