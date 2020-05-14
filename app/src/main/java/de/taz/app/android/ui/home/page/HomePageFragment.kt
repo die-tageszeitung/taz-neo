c8175b0386fa0dcf02ccf4a24d1e9aa0cbaf672d
Felix Höffken <felix@ctrl.alt.coop>
2020-05-14 13:51:52 +0200

    Fix instanciation crashes

--- app/src/main/java/de/taz/app/android/ui/home/page/HomePageFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/home/page/HomePageFragment.kt
@@ -26,9 +26,9 @@ abstract class HomePageFragment(
 
     private val log by Log
 
-    private val apiService: ApiService = ApiService.getInstance()
-    private val dateHelper: DateHelper = DateHelper.getInstance()
-    private val issueRepository: IssueRepository = IssueRepository.getInstance()
+    private var apiService: ApiService? = null
+    private var dateHelper: DateHelper? = null
+    private var issueRepository: IssueRepository? = null
 
     private var lastRequestedDate = ""
 
@@ -42,6 +42,10 @@ abstract class HomePageFragment(
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = viewModel ?: HomePageViewModel()
+
+        apiService = ApiService.getInstance(context?.applicationContext)
+        dateHelper = DateHelper.getInstance(context?.applicationContext)
+        issueRepository = IssueRepository.getInstance(context?.applicationContext)
 
         viewModel?.apply {
             issueStubsLiveData.observeDistinct(
@@ -71,7 +75,7 @@ abstract class HomePageFragment(
     suspend fun getNextIssueMoments(date: String) {
         if (lastRequestedDate.isEmpty() || date <= lastRequestedDate) {
             log.debug("lastRequestedDate: $lastRequestedDate date: $date requested new issues")
-            lastRequestedDate = dateHelper.stringToStringWithDelta(
+            lastRequestedDate = dateHelper?.stringToStringWithDelta(
                 date, -NUMBER_OF_REQUESTED_MOMENTS
             ) ?: ""
 
@@ -95,8 +99,9 @@ abstract class HomePageFragment(
     suspend fun downloadNextIssues(date: String, limit: Int) {
         withContext(Dispatchers.IO) {
             try {
-                val issues = apiService.getIssuesByDate(issueDate = date, limit = limit)
-                issueRepository.save(issues)
+                apiService?.getIssuesByDate(issueDate = date, limit = limit)?.let {
+                    issueRepository?.save(it)
+                }
             } catch (e: ApiService.ApiServiceException.NoInternetException) {
                 ToastHelper.getInstance(activity?.applicationContext)
                     .showToast(R.string.toast_no_internet)
