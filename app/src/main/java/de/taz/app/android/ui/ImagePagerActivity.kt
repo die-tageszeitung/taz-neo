bb51e8af6445bba48b07962ae76ef068a99061c0
eike <eike@ctrl.alt.coop>
2020-06-24 16:59:24 +0200

    Applied review comments

--- app/src/main/java/de/taz/app/android/ui/ImagePagerActivity.kt
+++ app/src/main/java/de/taz/app/android/ui/ImagePagerActivity.kt
@@ -15,28 +15,25 @@ import de.taz.app.android.persistence.repository.SectionRepository
 import de.taz.app.android.ui.webview.IMAGE_NAME
 import de.taz.app.android.ui.webview.ImageFragment
 import de.taz.app.android.ui.webview.pager.ARTICLE_NAME
-import de.taz.app.android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class ImagePagerActivity : NightModeActivity(R.layout.activity_login) {
+class ImagePagerActivity : NightModeActivity(R.layout.activity_image_pager) {
 
     private lateinit var mPager: ViewPager2
-    val log by Log
     private var articleName: String? = null
     var imageName: String? = null
     var imageList: List<Image>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_image_pager)
 
         articleName = intent.extras?.getString(ARTICLE_NAME)
         imageName = intent.extras?.getString(IMAGE_NAME)
 
         // Instantiate a ViewPager and a PagerAdapter.
-        mPager = findViewById(R.id.fragment_image_pager)
+        mPager = findViewById(R.id.activity_image_pager)
 
         // The pager adapter, which provides the pages to the view pager widget.
         val pagerAdapter = ImagePagerAdapter(this)
