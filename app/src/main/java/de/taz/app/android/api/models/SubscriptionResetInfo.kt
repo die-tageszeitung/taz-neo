45679b104f2995d9eee84f345b9f0383d23d9715
Felix Höffken <felix@ctrl.alt.coop>
2020-07-02 17:40:04 +0200

    Update dependencies

--- app/src/main/java/de/taz/app/android/api/models/SubscriptionResetInfo.kt
+++ app/src/main/java/de/taz/app/android/api/models/SubscriptionResetInfo.kt
@@ -1,10 +1,14 @@
 package de.taz.app.android.api.models
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class SubscriptionResetInfo(
     val status: SubscriptionResetStatus,
     val mail: String? = null
 )
 
+@JsonClass(generateAdapter = false)
 enum class SubscriptionResetStatus {
     ok,
     invalidSubscriptionId,
