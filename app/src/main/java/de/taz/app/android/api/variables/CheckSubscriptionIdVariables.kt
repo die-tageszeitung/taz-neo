b73084ad66e0a2430f17ee8b765b0baabdb607fc
Felix Höffken <felix@ctrl.alt.coop>
2020-07-03 11:52:23 +0200

    Fix JsonAdapter generation

--- app/src/main/java/de/taz/app/android/api/variables/CheckSubscriptionIdVariables.kt
+++ app/src/main/java/de/taz/app/android/api/variables/CheckSubscriptionIdVariables.kt
@@ -1,7 +1,6 @@
 package de.taz.app.android.api.variables
 
 import com.squareup.moshi.JsonClass
-import com.squareup.moshi.Moshi
 import de.taz.app.android.singletons.JsonHelper
 
 @JsonClass(generateAdapter = true)
