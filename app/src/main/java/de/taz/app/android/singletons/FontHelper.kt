d73a61f9a4cf929906c4bd5584b33fdf4cadac52
Felix Höffken <felix@ctrl.alt.coop>
2020-07-16 16:18:57 +0200

    Recreate file if parsing fails

--- app/src/main/java/de/taz/app/android/singletons/FontHelper.kt
+++ app/src/main/java/de/taz/app/android/singletons/FontHelper.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import java.io.File
+import java.lang.RuntimeException
 
 const val CONVERTED_FONT_FOLDER = "convertedFonts"
 
@@ -52,7 +53,12 @@ class FontHelper private constructor(applicationContext: Context) : ViewModel() 
                     WoffConverter().convertToTTFByteArray(it.inputStream())
                 )
             }
-            Typeface.createFromFile(ttfFile)
+            try {
+                Typeface.createFromFile(ttfFile)
+            } catch (re: RuntimeException) {
+                ttfFile.delete()
+                fromFile(fileName)
+            }
         }
     }
 }
