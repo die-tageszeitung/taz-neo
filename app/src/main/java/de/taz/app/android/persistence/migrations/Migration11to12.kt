1911821df8b1ba8e9796906c9be844a21dca995a
Felix Höffken <felix@ctrl.alt.coop>
2020-08-17 16:11:55 +0200

    Correct Migration DB versions

--- app/src/main/java/de/taz/app/android/persistence/migrations/Migration11to12.kt
+++ app/src/main/java/de/taz/app/android/persistence/migrations/Migration11to12.kt
@@ -3,7 +3,7 @@ package de.taz.app.android.persistence.migrations
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-object Migration11to12 : Migration(10, 11) {
+object Migration11to12 : Migration(11, 12) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.apply {
             execSQL( "ALTER TABLE Issue ADD COLUMN moTime TEXT DEFAULT \"2020-08-17\";")
