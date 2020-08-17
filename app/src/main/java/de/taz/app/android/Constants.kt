2e5c304a1dbf4d8b3fd9e395c96cc7658e584c45
eike <eike@ctrl.alt.coop>
2020-08-17 13:43:50 +0200

    Rename files according the naming on dl.taz.de

--- app/src/main/java/de/taz/app/android/Constants.kt
+++ app/src/main/java/de/taz/app/android/Constants.kt
@@ -11,8 +11,8 @@ const val DEFAULT_NAV_DRAWER_FILE_NAME = "navButton.taz.normal.png"
 const val SUBSCRIPTION_EMAIL_ADDRESS = "digiabo@taz.de"
 const val WEBVIEW_DRAG_SENSITIVITY_FACTOR = 2
 const val WEBVIEW_HTML_FILE = "htmlFile"
-const val WEBVIEW_HTML_FILE_TERMS = "welcomeAgb.html"
-const val WEBVIEW_HTML_FILE_REVOCATION = "welcomeWiderruf.html"
+const val WEBVIEW_HTML_FILE_TERMS = "welcomeTerms.html"
+const val WEBVIEW_HTML_FILE_REVOCATION = "welcomeRevocation.html"
 const val WEEKEND_TYPEFACE_RESOURCE_FILE_NAME="knile-semibold-webfont.woff"
 
 // Preferences
