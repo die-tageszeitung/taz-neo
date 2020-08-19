78d97de7cac40d415028bc1dd4f89e88e04bd7fb
Felix Höffken <felix@ctrl.alt.coop>
2020-08-19 17:36:28 +0200

    Prevent unnecessary saves in more locations

--- app/src/main/java/de/taz/app/android/singletons/SubscriptionPollHelper.kt
+++ app/src/main/java/de/taz/app/android/singletons/SubscriptionPollHelper.kt
@@ -11,7 +11,6 @@ import de.taz.app.android.monkey.observeDistinct
 import de.taz.app.android.monkey.observeDistinctOnce
 import de.taz.app.android.persistence.repository.IssueRepository
 import de.taz.app.android.util.SingletonHolder
-import io.sentry.Sentry
 import kotlinx.coroutines.*
 
 class SubscriptionPollHelper private constructor(applicationContext: Context) : ViewModel() {
