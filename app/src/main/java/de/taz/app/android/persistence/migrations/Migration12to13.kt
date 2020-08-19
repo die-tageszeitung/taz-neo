4fd4cc954d2aaab940e33f5d502166031b93e756
Felix Höffken <felix@ctrl.alt.coop>
2020-08-19 16:21:26 +0200

    Set correct migration versions

--- app/src/main/java/de/taz/app/android/persistence/migrations/Migration12to13.kt
+++ app/src/main/java/de/taz/app/android/persistence/migrations/Migration12to13.kt
@@ -3,7 +3,7 @@ package de.taz.app.android.persistence.migrations
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-object Migration12to13 : Migration(11, 12) {
+object Migration12to13 : Migration(12, 13) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.apply {
             execSQL("CREATE TABLE IF NOT EXISTS TMPDownload (`fileName` TEXT NOT NULL, `baseUrl` TEXT NOT NULL, `status` TEXT NOT NULL, `lastSha256` TEXT, PRIMARY KEY(`fileName`))")
