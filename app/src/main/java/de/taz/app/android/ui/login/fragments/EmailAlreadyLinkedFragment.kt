539ea132450472c208a4e47c96d7001ac078699f
Felix Höffken <felix@ctrl.alt.coop>
2020-09-03 16:47:29 +0200

    Set correct state after tried login

--- app/src/main/java/de/taz/app/android/ui/login/fragments/EmailAlreadyLinkedFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/login/fragments/EmailAlreadyLinkedFragment.kt
@@ -12,7 +12,11 @@ class EmailAlreadyLinkedFragment : LoginBaseFragment(R.layout.fragment_login_ema
         super.onViewCreated(view, savedInstanceState)
 
         fragment_login_email_already_taken_insert_new.setOnClickListener {
-            viewModel.status.postValue(LoginViewModelState.CREDENTIALS_MISSING_LOGIN)
+            viewModel.apply {
+                username = ""
+                password = ""
+                status.postValue(LoginViewModelState.SUBSCRIPTION_ACCOUNT)
+            }
         }
         fragment_login_email_already_taken_contact_email.setOnClickListener {
             writeEmail()
