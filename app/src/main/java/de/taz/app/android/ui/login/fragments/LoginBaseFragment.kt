1a72f16de306c752bbf58bcfde481cd58450172c
Felix Höffken <felix@ctrl.alt.coop>
2020-09-04 13:28:27 +0200

    Add more help texts

--- app/src/main/java/de/taz/app/android/ui/login/fragments/LoginBaseFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/login/fragments/LoginBaseFragment.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.annotation.LayoutRes
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import de.taz.app.android.SUBSCRIPTION_EMAIL_ADDRESS
 import de.taz.app.android.ui.login.LoginViewModel
 
@@ -36,6 +38,19 @@ abstract class LoginBaseFragment(@LayoutRes layoutId: Int): Fragment(layoutId) {
     override fun onDetach() {
         hideKeyBoard()
         super.onDetach()
+    }
+
+    fun showHelpDialog(@StringRes stringRes: Int) {
+        context?.let {
+            val dialog = MaterialAlertDialogBuilder(it)
+                .setMessage(stringRes)
+                .setPositiveButton(android.R.string.ok) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+
+            dialog.show()
+        }
     }
 
 }
