f792b3a374fc1c97ddbf300ace1c7bd859fad903
Felix Höffken <felix@ctrl.alt.coop>
2020-09-11 14:52:10 +0200

    Add deviceOS information

--- app/src/main/java/de/taz/app/android/api/variables/CheckSubscriptionIdVariables.kt
+++ app/src/main/java/de/taz/app/android/api/variables/CheckSubscriptionIdVariables.kt
@@ -14,7 +14,8 @@ data class CheckSubscriptionIdVariables(
     val deviceVersion: String? = android.os.Build.VERSION.RELEASE,
     val appVersion: String = BuildConfig.VERSION_NAME,
     val deviceFormat: DeviceFormat = DeviceFormat.mobile,
-    val deviceType: DeviceType = DeviceType.android
+    val deviceType: DeviceType = DeviceType.android,
+    val deviceOS: String? = System.getProperty("os.version"),
 ) : Variables {
     override fun toJson() = JsonHelper.toJson(this)
 }
