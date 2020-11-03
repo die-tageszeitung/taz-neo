d447f1f713a23b137b350abdb1dd9033218cba86
eike <eike@ctrl.alt.coop>
2020-11-03 10:23:21 +0100

    Adopt graphQl to get the momentList
    * momentList has html and gif for the animated moment
    * database table IsseFilesMomentJoin was created

--- app/src/main/java/de/taz/app/android/api/dto/MomentDto.kt
+++ app/src/main/java/de/taz/app/android/api/dto/MomentDto.kt
@@ -1,9 +1,11 @@
 package de.taz.app.android.api.dto
 
 import com.squareup.moshi.JsonClass
+import de.taz.app.android.api.models.FileEntry
 
 @JsonClass(generateAdapter = true)
 data class MomentDto(
     val imageList: List<ImageDto>? = null,
-    val creditList: List<ImageDto>? = null
+    val creditList: List<ImageDto>? = null,
+    val momentList: List<FileEntryDto>? = null
 )
