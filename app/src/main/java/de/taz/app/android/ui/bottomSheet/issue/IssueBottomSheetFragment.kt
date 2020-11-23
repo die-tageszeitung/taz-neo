b061336ed649e5d0411db87c18fcc81af59298e4
eike <eike@ctrl.alt.coop>
2020-11-23 15:07:34 +0100

    Add PdfRenderActivity to show PDF

--- app/src/main/java/de/taz/app/android/ui/bottomSheet/issue/IssueBottomSheetFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/bottomSheet/issue/IssueBottomSheetFragment.kt
@@ -3,6 +3,7 @@ package de.taz.app.android.ui.bottomSheet.issue
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
