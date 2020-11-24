f61659448d8f059c16b790933975a57512eb2ea7
eike <eike@ctrl.alt.coop>
2020-11-24 18:17:37 +0100

    Add all pdf's from issue to the list

--- app/src/main/java/de/taz/app/android/ui/PdfFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/PdfFragment.kt
@@ -1,23 +1,25 @@
 package de.taz.app.android.ui
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
 import android.os.ParcelFileDescriptor
 import androidx.fragment.app.Fragment
 import de.taz.app.android.R
 import de.taz.app.android.util.Log
-import kotlinx.android.synthetic.main.activity_pdf_renderer.*
 import kotlinx.android.synthetic.main.fragment_pdf_viewer.*
 import java.io.File
 import java.io.IOException
 
 class PdfFragment: Fragment(R.layout.fragment_pdf_viewer) {
+
     val log by Log
     var file: File? = null
+    private var pageIndex = 0
     private var pdfRenderer: PdfRenderer? = null
     private var currentPage: PdfRenderer.Page? = null
     private var parcelFileDescriptor: ParcelFileDescriptor? = null
+    private val pageCount: Int
+        get() = pdfRenderer!!.pageCount
 
     companion object {
         fun createInstance(
@@ -30,33 +32,36 @@ class PdfFragment: Fragment(R.layout.fragment_pdf_viewer) {
     }
 
     override fun onStart() {
-        log.debug("onStart!!! file? ${file?.exists()} context? ${context?.applicationContext}")
+        log.debug("onStart!!! file? ${file?.exists()}")
         super.onStart()
-        context?.applicationContext?.let {
-            try {
-                openRenderer(it)
-                showPage()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
+        try {
+            openRenderer()
+            showPage(pageIndex)
+        } catch (e: IOException) {
+            log.error("Something went wrong opening pdf renderer: ${e.localizedMessage}")
         }
     }
 
-    override fun onStop() {
-        log.debug("onStop!!!")
+    override fun onDestroyView() {
         try {
             closeRenderer()
         } catch (e: IOException) {
-            e.printStackTrace()
+            log.error("Something went wrong closing pdf renderer: ${e.localizedMessage}")
         }
-        super.onStop()
+        super.onDestroyView()
     }
 
-    @Throws(IOException::class)
-    private fun openRenderer(context: Context) {
+    fun onPreviousDocClick() {
+        showPage(currentPage!!.index - 1)
+    }
+
+    fun onNextDocClick() {
+        showPage(currentPage!!.index + 1)
+    }
+
+    private fun openRenderer() {
         parcelFileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
         // This is the PdfRenderer we use to render the PDF.
-        log.debug("pardel: $parcelFileDescriptor")
         if (parcelFileDescriptor != null) {
             pdfRenderer = PdfRenderer(parcelFileDescriptor!!)
         }
@@ -70,7 +75,12 @@ class PdfFragment: Fragment(R.layout.fragment_pdf_viewer) {
         parcelFileDescriptor!!.close()
     }
 
-    private fun showPage() {
+    private fun showPage(index: Int) {
+        if (pdfRenderer!!.pageCount <= index) {
+            return
+        }
+
+        log.debug("showing page $index of $pageCount!!!")
 
         // Make sure to close the current page before opening another one.
         if (null != currentPage) {
@@ -78,7 +88,7 @@ class PdfFragment: Fragment(R.layout.fragment_pdf_viewer) {
         }
 
         // Use `openPage` to open a specific page in PDF.
-        currentPage = pdfRenderer!!.openPage(0)
+        currentPage = pdfRenderer!!.openPage(index)
 
         // Important: the destination bitmap must be ARGB (not RGB).
         val bitmap = Bitmap.createBitmap(
@@ -92,6 +102,6 @@ class PdfFragment: Fragment(R.layout.fragment_pdf_viewer) {
         // Pass either RENDER_MODE_FOR_DISPLAY or RENDER_MODE_FOR_PRINT for the last parameter.
         currentPage!!.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
         // We are ready to show the Bitmap to user.
-        pdf_image.setImageBitmap(bitmap)
+        pdf_image?.setImageBitmap(bitmap)
     }
 }
