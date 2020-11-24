81cf83d4a32c9e35be70d22d18d195d5254bb3df
eike <eike@ctrl.alt.coop>
2020-11-24 17:11:05 +0100

    Add fragment and move logic of pdf renderer to it

--- app/src/main/java/de/taz/app/android/ui/PdfRenderActivity.kt
+++ app/src/main/java/de/taz/app/android/ui/PdfRenderActivity.kt
@@ -1,131 +1,84 @@
 package de.taz.app.android.ui
 
-import de.taz.app.android.R
-import android.os.Bundle
-import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.pdf.PdfRenderer
-import android.os.ParcelFileDescriptor
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import de.taz.app.android.R
+import de.taz.app.android.api.models.Issue
+import de.taz.app.android.api.models.IssueStatus
 import de.taz.app.android.base.NightModeActivity
+import de.taz.app.android.monkey.reduceDragSensitivity
+import de.taz.app.android.persistence.repository.IssueRepository
 import de.taz.app.android.singletons.FileHelper
 import de.taz.app.android.util.Log
 import io.sentry.core.Sentry
-import kotlinx.android.synthetic.main.activity_pdf_renderer.*
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.File
-import java.io.IOException
 
 class PdfRenderActivity : NightModeActivity(R.layout.activity_pdf_renderer) {
 
     val log by Log
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var pagerAdapter: PdfRenderActivity.PdfPagerAdapter
     private var file: File? = null
-    private var pageIndex = 0
-    private var pdfRenderer: PdfRenderer? = null
-    private var currentPage: PdfRenderer.Page? = null
-    private var parcelFileDescriptor: ParcelFileDescriptor? = null
+    private var issue: Issue? = null
+    private var pdfList: List<File> = emptyList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        log.debug("hello from rendererActivity!!!!!!")
 
         val fileHelper = FileHelper.getInstance()
         try {
-            val fileName = intent.extras?.getString(Intent.EXTRA_TEXT)
-            CoroutineScope(Dispatchers.IO).launch {
-                file = fileName?.let { fileHelper.getFile(it) }
+
+            // Instantiate a ViewPager
+            viewPager2 = findViewById(R.id.activity_pdf_pager)
+
+            // Instantiate pager adapter, which provides the pages to the view pager widget.
+            pagerAdapter = PdfPagerAdapter(this@PdfRenderActivity)
+            viewPager2.adapter = pagerAdapter
+
+            viewPager2.apply {
+                reduceDragSensitivity(6)
+                offscreenPageLimit = 2
             }
+
+            // TODO: get date and feed from intent eg = intent.extras?.getString(Intent.EXTRA_TEXT)
+            runBlocking(Dispatchers.IO) {
+                issue = IssueRepository.getInstance().getIssueByFeedAndDate(
+                    "taz", "2020-11-24", IssueStatus.regular
+                )
+                pdfList = issue?.pageList?.map {
+                    fileHelper.getFile(it.pagePdf)
+                } ?: emptyList()
+                log.debug("first of pdfList: ${pdfList.first().name}!!!")
+            }
+
         } catch (e: NullPointerException) {
             val hint = "no FILENAME given as parameter, finishing PdfRendererActivity"
             log.error(hint)
             Sentry.captureMessage(hint)
             finish()
         }
-        log.debug("file exists? ${file?.exists()}")
 
-        setContentView(R.layout.activity_pdf_renderer)
-        pageIndex = 0
     }
 
-    override fun onStart() {
-        super.onStart()
-        try {
-            openRenderer(applicationContext)
-            showPage(pageIndex)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-
-    public override fun onStop() {
-        try {
-            closeRenderer()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        super.onStop()
-    }
-
-    fun onPreviousDocClick() {
-        showPage(currentPage!!.index - 1)
-    }
-
-    fun onNextDocClick() {
-        showPage(currentPage!!.index + 1)
-    }
-
-    @Throws(IOException::class)
-    private fun openRenderer(context: Context) {
-        parcelFileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
-        // This is the PdfRenderer we use to render the PDF.
-        if (parcelFileDescriptor != null) {
-            pdfRenderer = PdfRenderer(parcelFileDescriptor!!)
-        }
-    }
-
-    private fun closeRenderer() {
-        if (null != currentPage) {
-            currentPage!!.close()
-        }
-        pdfRenderer!!.close()
-        parcelFileDescriptor!!.close()
-    }
-
-    private fun showPage(index: Int) {
-        if (pdfRenderer!!.pageCount <= index) {
-            return
+    /**
+     * A simple pager adapter that represents pdfFragment objects, in
+     * sequence.
+     */
+    private inner class PdfPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun createFragment(position: Int): Fragment {
+            return PdfFragment.createInstance(
+                pdfList.first()
+            )
         }
 
-        // Make sure to close the current page before opening another one.
-        if (null != currentPage) {
-            currentPage!!.close()
+        override fun getItemCount(): Int {
+            return pdfList.size
         }
-
-        // Use `openPage` to open a specific page in PDF.
-        currentPage = pdfRenderer!!.openPage(index)
-
-        // Important: the destination bitmap must be ARGB (not RGB).
-        val bitmap = Bitmap.createBitmap(
-            currentPage!!.width,
-            currentPage!!.height,
-            Bitmap.Config.ARGB_8888
-        )
-        // Here, we render the page onto the Bitmap.
-        // To render a portion of the page, use the second and third parameter. Pass nulls to get
-        // the default result.
-        // Pass either RENDER_MODE_FOR_DISPLAY or RENDER_MODE_FOR_PRINT for the last parameter.
-        currentPage!!.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
-        // We are ready to show the Bitmap to user.
-        pdf_image.setImageBitmap(bitmap)
-    }
-
-
-    val pageCount: Int
-        get() = pdfRenderer!!.pageCount
-
-    companion object {
-        private const val FILENAME = "report.pdf"
     }
 }
