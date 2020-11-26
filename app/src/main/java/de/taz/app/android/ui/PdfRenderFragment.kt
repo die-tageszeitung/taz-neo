1e09689c3b69ca7f2ce84bd74951c283fb94d0f8
eike <eike@ctrl.alt.coop>
2020-11-26 15:20:49 +0100

    Add click logic to get link from frameList

--- app/src/main/java/de/taz/app/android/ui/PdfRenderFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/PdfRenderFragment.kt
@@ -1,36 +1,48 @@
 package de.taz.app.android.ui
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
 import android.os.ParcelFileDescriptor
 import androidx.fragment.app.Fragment
 import de.taz.app.android.R
+import de.taz.app.android.api.models.Frame
 import de.taz.app.android.util.Log
 import kotlinx.android.synthetic.main.fragment_pdf_render.*
 import java.io.File
 import java.io.IOException
 
-class PdfRenderFragment: Fragment(R.layout.fragment_pdf_render) {
+class PdfRenderFragment : Fragment(R.layout.fragment_pdf_render) {
 
     val log by Log
-    var file: File? = null
+    var pdfPage: File? = null
+    var frameList: List<Frame> = emptyList()
     private var pageIndex = 0
     private var pdfRenderer: PdfRenderer? = null
     private var currentPage: PdfRenderer.Page? = null
     private var parcelFileDescriptor: ParcelFileDescriptor? = null
 
+    // Math stuff
+    var startX = 0f
+    var startY = 0f
+
     companion object {
         fun createInstance(
-            file: File?
+            pdfPageWithFrameList: Pair<File, List<Frame>?>
         ): PdfRenderFragment {
             val fragment = PdfRenderFragment()
-            fragment.file = file
+            fragment.pdfPage = pdfPageWithFrameList.first
+            fragment.frameList = pdfPageWithFrameList.second ?: emptyList()
             return fragment
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onStart() {
         super.onStart()
+        pdf_photo_view?.setOnPhotoTapListener { _, x, y ->
+            showFrameIfPossible(x, y)
+        }
         try {
             openRenderer()
             showPage(pageIndex)
@@ -39,17 +51,18 @@ class PdfRenderFragment: Fragment(R.layout.fragment_pdf_render) {
         }
     }
 
-    override fun onDestroyView() {
+    override fun onStop() {
         try {
             closeRenderer()
         } catch (e: IOException) {
             log.error("Something went wrong closing pdf renderer: ${e.localizedMessage}")
         }
-        super.onDestroyView()
+        super.onStop()
     }
 
     private fun openRenderer() {
-        parcelFileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
+        parcelFileDescriptor =
+            ParcelFileDescriptor.open(pdfPage, ParcelFileDescriptor.MODE_READ_ONLY)
 
         // This is the PdfRenderer we use to render the PDF
         if (parcelFileDescriptor != null) {
@@ -87,5 +100,12 @@ class PdfRenderFragment: Fragment(R.layout.fragment_pdf_render) {
 
         // We are ready to show the Bitmap to user
         pdf_photo_view?.setImageBitmap(bitmap)
+    }
+
+    private fun showFrameIfPossible(x: Float, y: Float) {
+        log.debug("Clicked on x: $x   y:$y")
+        frameList.forEach { log.verbose("possible frame: $it") }
+        val frame = frameList.firstOrNull { it.x1 <= x && x < it.x2 && it.y1 <= y && y < it.y2 }
+        log.debug("found frame with link: ${frame?.link}")
     }
 }
