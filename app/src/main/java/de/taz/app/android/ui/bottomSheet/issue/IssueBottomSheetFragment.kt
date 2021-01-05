f78e66ba0cf7bbcfd13dd09aa0e76f4166cf11b7
eike <eike@ctrl.alt.coop>
2021-01-05 16:26:24 +0100

    Add mu pdf
    and use it instead of photo view

--- app/src/main/java/de/taz/app/android/ui/bottomSheet/issue/IssueBottomSheetFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/bottomSheet/issue/IssueBottomSheetFragment.kt
@@ -26,7 +26,6 @@ import de.taz.app.android.simpleDateFormat
 import de.taz.app.android.singletons.StorageService
 import de.taz.app.android.singletons.ToastHelper
 import de.taz.app.android.ui.PdfPagerActivity
-import de.taz.app.android.ui.bookmarks.BookmarkListActivity
 import de.taz.app.android.ui.home.page.HomePageViewModel
 import de.taz.app.android.ui.issueViewer.IssueViewerActivity
 import de.taz.app.android.util.Log
