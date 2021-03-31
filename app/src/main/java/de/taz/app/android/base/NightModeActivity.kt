06e3d46196c66fbe2b670db3c3af748c0c939976
Jonatan Zint <jonatan@ctrl.alt.coop>
2021-03-31 16:18:48 +0200

    Fix not-applied night mode setting after restarting app
    As we only react to changing the night mode preference we still need to set the current theme during activity startup

--- app/src/main/java/de/taz/app/android/base/NightModeActivity.kt
+++ app/src/main/java/de/taz/app/android/base/NightModeActivity.kt
@@ -3,7 +3,9 @@ package de.taz.app.android.base
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import de.taz.app.android.PREFERENCES_TAZAPICSS
+import de.taz.app.android.singletons.SETTINGS_TEXT_NIGHT_MODE
 import de.taz.app.android.util.NightModeHelper
 
 abstract class NightModeActivity(layoutId: Int? = null): BaseActivity(layoutId) {
@@ -18,9 +20,15 @@ abstract class NightModeActivity(layoutId: Int? = null): BaseActivity(layoutId) 
         tazApiCssPreferences =
             applicationContext.getSharedPreferences(PREFERENCES_TAZAPICSS, Context.MODE_PRIVATE)
 
-        NightModeHelper.initializeNightModePrefs(tazApiCssPreferences, this)
+        // Set initial theme by reading the preferences
+        val darkModeEnabled = tazApiCssPreferences.getBoolean(SETTINGS_TEXT_NIGHT_MODE, false)
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkModeEnabled) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        )
 
+        NightModeHelper.initializeNightModePrefs(tazApiCssPreferences, this)
         tazApiCssPrefListener = NightModeHelper.PrefListener(this).tazApiCssPrefListener
+
     }
 
     override fun onResume() {
