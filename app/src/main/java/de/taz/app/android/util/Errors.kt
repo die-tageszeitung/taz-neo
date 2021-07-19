c554404cba2284da091687ea37aad38d831830c8
Felix Höffken <felix@ctrl.alt.coop>
2021-07-19 14:32:34 +0200

    Remove unused reportAndRethrowExceptionsAsync fun

--- app/src/main/java/de/taz/app/android/util/Errors.kt
+++ app/src/main/java/de/taz/app/android/util/Errors.kt
@@ -2,17 +2,6 @@ package de.taz.app.android.util
 
 import io.sentry.Sentry
 
-
-suspend fun <T> reportAndRethrowExceptionsAsync(block: suspend () -> T): T {
-    try {
-        return block()
-    } catch (e: Exception) {
-        Sentry.captureException(e)
-        throw e
-    }
-}
-
-
 fun <T> reportAndRethrowExceptions(block: () -> T): T {
     try {
         return block()
