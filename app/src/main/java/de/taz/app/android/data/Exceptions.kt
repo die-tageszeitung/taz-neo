1a9e9c210164bc4fa1c1940b09cae570ceaff387
Felix Höffken <felix@ctrl.alt.coop>
2021-07-19 14:34:16 +0200

    Remove unused DataExceptions

--- app/src/main/java/de/taz/app/android/data/Exceptions.kt
+++
@@ -1,9 +0,0 @@
-package de.taz.app.android.data
-
-import java.lang.Exception
-
-
-sealed class DataException(message: String, override val cause: Throwable?): Exception(message, cause) {
-    class NoValidSubscriptionException(message: String = "No valid subscription", cause: Throwable? = null): DataException(message, cause)
-    class InconsistentRequestException(message: String = "The request cannot be fulfilled because it's parameters are inconsistent", cause: Throwable? = null): DataException(message, cause)
-}
