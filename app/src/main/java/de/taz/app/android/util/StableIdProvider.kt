255daedb7584cc7666ada90ea2662a3650c5042d
Felix Höffken <felix@ctrl.alt.coop>
2021-07-19 14:25:30 +0200

    Remove unused StableIdViewModel

--- app/src/main/java/de/taz/app/android/util/StableIdProvider.kt
+++ app/src/main/java/de/taz/app/android/util/StableIdProvider.kt
@@ -5,17 +5,3 @@ import androidx.lifecycle.ViewModel
 interface StableIdProvider {
     fun getId(key: String): Long
 }
-
-class StableIdViewModel: ViewModel(),
-    StableIdProvider {
-    private val keyList = mutableListOf<String>()
-
-    override fun getId(key: String): Long {
-        var position = keyList.indexOf(key)
-        if (position < 0) {
-            position = keyList.size
-            keyList.add(key)
-        }
-        return position.toLong()
-    }
-}
