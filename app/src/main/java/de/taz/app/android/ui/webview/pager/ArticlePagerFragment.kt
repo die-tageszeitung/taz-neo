1fb19625c366b5b7dfacd5eedf1a0cf96a29a15c
Johannes Dillmann <johannes@ctrl.alt.coop>
2021-09-01 15:00:44 +0200

    Add comment

--- app/src/main/java/de/taz/app/android/ui/webview/pager/ArticlePagerFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/webview/pager/ArticlePagerFragment.kt
@@ -259,6 +259,10 @@ class ArticlePagerFragment : BaseMainFragment(
     override fun onDestroyView() {
         webview_pager_viewpager.adapter = null
         if (this.tag == ARTICLE_PAGER_FRAGMENT_FROM_PDF_MODE) {
+            // On device orientation changes the Fragments Activity is already destroyed when we reach the onDestroyView method.
+            // Thus we cant initialize a ViewModel instance from onDestroyView. 
+            // As the `by activityViewModels()` is called lazily and not being used before, the ViewModel can not be initialized.
+            // To prevent the app from crashing in this case we check explicitly that the Activity has not been destroyed yet.
             if(!this.requireActivity().isDestroyed)
                 pdfPagerViewModel.hideDrawerLogo.postValue(true)
         }
