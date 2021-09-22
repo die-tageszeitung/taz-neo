8d78d0eb33f2324b9c17fb32397b05353a51ed13
Felix Höffken <felix@ctrl.alt.coop>
2021-09-22 10:44:03 +0200

    Correctly migrate database

--- app/src/main/java/de/taz/app/android/persistence/migrations/Migration21to22.kt
+++ app/src/main/java/de/taz/app/android/persistence/migrations/Migration21to22.kt
@@ -6,7 +6,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 object Migration21to22 : Migration(21, 22) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.apply {
-            execSQL("ALTER TABLE Issue ADD COLUMN lastPageName INT;")
+            execSQL("ALTER TABLE Issue ADD COLUMN lastPagePosition INT;")
         }
     }
 }
