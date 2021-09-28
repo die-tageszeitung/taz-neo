241a2b265f476cd492c8e0ea7f3d3d3ee16432bc
Felix Höffken <felix@ctrl.alt.coop>
2021-09-28 14:41:31 +0200

    Use use to prevent not closing fos

--- app/src/main/java/de/taz/app/android/util/PDFThumbnailLoader.kt
+++ app/src/main/java/de/taz/app/android/util/PDFThumbnailLoader.kt
@@ -70,13 +70,14 @@ class PDFThumbnailLoader(private val context: Context): ModelLoader<String, Bitm
                 if (!thumbnail.exists()) {
                     // create thumbnail for first page of pdf file
                     output = MuPDFThumbnail(input).thumbnail(width)
-                    val fos = FileOutputStream(thumbnail)
-                    output.compress(
-                        Bitmap.CompressFormat.PNG,
-                        100,
-                        fos
-                    )
-                    fos.close()
+                    FileOutputStream(thumbnail).use { fos ->
+                        output.compress(
+                            Bitmap.CompressFormat.PNG,
+                            100,
+                            fos
+                        )
+                        fos.close()
+                    }
                 } else {
                     output = BitmapFactory.decodeFile(thumbnail.absolutePath)
                 }
