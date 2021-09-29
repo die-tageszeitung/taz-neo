c680bdcda7de1f520bf0fbca533e65af0050c842
Felix Höffken <felix@ctrl.alt.coop>
2021-09-29 17:28:29 +0200

    Remove functions from DataStoreEntry interface

--- app/src/main/java/de/taz/app/android/dataStore/DataStoreEntry.kt
+++ app/src/main/java/de/taz/app/android/dataStore/DataStoreEntry.kt
@@ -11,8 +11,7 @@ import kotlinx.coroutines.flow.map
 
 
 interface DataStoreEntry<T> {
-    fun asFlow(): Flow<T>
-    fun asLiveData(): LiveData<T> = asFlow().asLiveData()
+    fun asLiveData(): LiveData<T>
 
     /**
      * function to set the new value
@@ -21,15 +20,9 @@ interface DataStoreEntry<T> {
     suspend fun set(value: T)
 
     /**
-     * function to check whether a value has been stored in the database
-     * @return true if a value exists in the database, false otherwise
-     */
-    suspend fun hasBeenSet(): Boolean
-
-    /**
      * function to get the currently set value - or the default
      */
-    suspend fun get(): T = asFlow().first()
+    suspend fun get(): T
 
     /**
      * function to reset DataSoreEntry to default value
@@ -43,32 +36,15 @@ class SimpleDataStoreEntry<T>(
     private val key: Preferences.Key<T>,
     private val default: T
 ) : DataStoreEntry<T> {
-    override fun asFlow(): Flow<T> = dataStore.data.map { it[key] ?: default }
+    private fun asFlow(): Flow<T> = dataStore.data.map { it[key] ?: default }
+
+    override fun asLiveData(): LiveData<T> = asFlow().asLiveData()
+
+    override suspend fun get(): T = asFlow().first()
 
     override suspend fun set(value: T) {
         dataStore.edit { it[key] = value }
     }
-
-    override suspend fun hasBeenSet(): Boolean =
-        dataStore.data.map { it[key] }.first() != null
-
-    override suspend fun reset() = set(default)
-}
-
-class MappingDataStoreEntry<S, T>(
-    dataStore: DataStore<Preferences>,
-    key: Preferences.Key<T>,
-    private val default: S,
-    private val mapStoT: (S) -> T,
-    private val mapTtoS: (T) -> S
-) : DataStoreEntry<S> {
-    private val dataStoreEntry = SimpleDataStoreEntry(dataStore, key, mapStoT(default))
-
-    override fun asFlow(): Flow<S> = dataStoreEntry.asFlow().map { mapTtoS(it) }
-
-    override suspend fun set(value: S) = dataStoreEntry.set(mapStoT(value))
-
-    override suspend fun hasBeenSet(): Boolean = dataStoreEntry.hasBeenSet()
 
     override suspend fun reset() = set(default)
 }
