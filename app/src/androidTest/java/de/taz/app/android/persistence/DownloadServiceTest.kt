516775c1e6808d3633ad41e68b211a5e02779e6d
Felix Höffken <felix@ctrl.alt.coop>
2021-10-13 16:46:51 +0200

    Fix DownloadServiceTest

--- app/src/androidTest/java/de/taz/app/android/persistence/DownloadServiceTest.kt
+++ app/src/androidTest/java/de/taz/app/android/persistence/DownloadServiceTest.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import de.taz.app.android.api.ApiService
+import de.taz.app.android.dataStore.DownloadDataStore
 import de.taz.app.android.download.DownloadService
 import de.taz.app.android.persistence.repository.FileEntryRepository
 import de.taz.app.android.persistence.repository.IssueRepository
 import de.taz.app.android.singletons.StorageService
 import io.ktor.client.*
 import io.ktor.client.engine.mock.*
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -34,12 +36,13 @@ class DownloadServiceTest {
             IssueRepository.getInstance(context),
             ApiService.getInstance(context),
             StorageService.getInstance(context),
-            mockHttpClient
+            mockHttpClient,
+            DownloadDataStore.getInstance(context)
         )
     }
 
     @Test
-    fun pollNewIssue() {
+    fun pollNewIssue() = runBlocking {
         val operation = downloadService.scheduleNewestIssueDownload("test", true)
         Assert.assertNotNull(operation)
     }
