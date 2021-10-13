62be28a2931a2a08fdfdd2a42126e1fdbc56139d
Felix Höffken <felix@ctrl.alt.coop>
2021-10-13 14:35:43 +0200

    Omit runBlocking in ErrorReportFramgent

--- app/src/main/java/de/taz/app/android/ui/settings/support/ErrorReportFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/settings/support/ErrorReportFragment.kt
@@ -8,6 +8,7 @@ import android.util.Base64
 import android.view.MenuItem
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.lifecycleScope
 import de.taz.app.android.MAX_BYTES
 import de.taz.app.android.R
 import de.taz.app.android.api.ApiService
@@ -22,7 +23,6 @@ import kotlinx.android.synthetic.main.fragment_header_default.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import java.lang.Exception
 import java.util.*
 
@@ -49,16 +49,18 @@ class ErrorReportFragment : BaseMainFragment(R.layout.fragment_error_report) {
 
         view.apply {
             fragment_header_default_title.text =
-                getString(R.string.settings_header).toLowerCase(Locale.GERMAN)
+                getString(R.string.settings_header).lowercase(Locale.GERMAN)
 
-            // read email from settings
-            fragment_error_report_email.setText(
-                runBlocking { AuthHelper.getInstance(requireContext().applicationContext).email.get() }
-            )
+            lifecycleScope.launch {
+                // read email from settings
+                fragment_error_report_email.setText(
+                    AuthHelper.getInstance(requireContext().applicationContext).email.get()
+                )
+            }
 
-            fragment_error_report_upload.setOnClickListener(View.OnClickListener {
+            fragment_error_report_upload.setOnClickListener {
                 getImageFromGallery.launch("image/*")
-            })
+            }
 
             fragment_error_report_send_button.setOnClickListener {
                 loading_screen.visibility = View.VISIBLE
@@ -137,7 +139,8 @@ class ErrorReportFragment : BaseMainFragment(R.layout.fragment_error_report) {
         screenshot: String?
     ) {
         val storageType =
-            StorageService.getInstance(activity?.applicationContext).getInternalFilesDir().absolutePath
+            StorageService.getInstance(activity?.applicationContext)
+                .getInternalFilesDir().absolutePath
         val errorProtocol = Log.trace.toString()
 
         val activityManager =
