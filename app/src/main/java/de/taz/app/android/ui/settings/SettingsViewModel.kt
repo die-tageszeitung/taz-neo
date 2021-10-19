12e3d78eeb26776b5d9f36f3d4194f1b66ef34c3
Felix Höffken <felix@ctrl.alt.coop>
2021-10-19 14:15:25 +0200

    Use StorageDataStore in SettingsFragment and Viewmodel

--- app/src/main/java/de/taz/app/android/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/de/taz/app/android/ui/settings/SettingsViewModel.kt
@@ -1,14 +1,13 @@
 package de.taz.app.android.ui.settings
 
 import android.app.Application
-import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import de.taz.app.android.*
+import de.taz.app.android.api.interfaces.StorageLocation
 import de.taz.app.android.dataStore.DownloadDataStore
+import de.taz.app.android.dataStore.StorageDataStore
 import de.taz.app.android.dataStore.TazApiCssDataStore
-import de.taz.app.android.util.SharedPreferenceStorageLocationLiveData
-import de.taz.app.android.util.SharedPreferenceStringLiveData
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -21,34 +20,35 @@ class SettingsViewModel(application: Application) : AndroidViewModel(application
     val downloadOnlyWifiLiveData: LiveData<Boolean>
     val  downloadAutomaticallyLiveData: LiveData<Boolean>
 
-    lateinit var storageLocationLiveData: SharedPreferenceStorageLocationLiveData
-    lateinit var storedIssueNumberLiveData: SharedPreferenceStringLiveData
+    var storageLocationLiveData: LiveData<StorageLocation>
+    var storedIssueNumberLiveData: LiveData<Int>
 
     private val tazApiCssDataStore = TazApiCssDataStore.getInstance(application)
     private val downloadDataStore = DownloadDataStore.getInstance(application)
+    private val storageDataStore = StorageDataStore.getInstance(application)
 
     init {
         fontSizeLiveData = tazApiCssDataStore.fontSize.asLiveData()
 
         nightModeLiveData = tazApiCssDataStore.nightMode.asLiveData()
 
-        application.getSharedPreferences(PREFERENCES_GENERAL, Context.MODE_PRIVATE)?.let {
-            storedIssueNumberLiveData =
-                SharedPreferenceStringLiveData(
-                    it,
-                    SETTINGS_GENERAL_KEEP_ISSUES,
-                    SETTINGS_GENERAL_KEEP_ISSUES_DEFAULT.toString()
-                )
-            storageLocationLiveData =
-                SharedPreferenceStorageLocationLiveData(
-                    it,
-                    SETTINGS_GENERAL_STORAGE_LOCATION,
-                    SETTINGS_GENERAL_STORAGE_LOCATION_DEFAULT
-                )
-        }
+        storedIssueNumberLiveData = storageDataStore.keepIssuesNumber.asLiveData()
+        storageLocationLiveData = storageDataStore.storageLocation.asLiveData()
 
         downloadOnlyWifiLiveData = downloadDataStore.onlyWifi.asLiveData()
         downloadAutomaticallyLiveData = downloadDataStore.enabled.asLiveData()
+    }
+
+    fun setStorageLocation(storageLocation: StorageLocation?) {
+        if (storageLocation != null) {
+            CoroutineScope(Dispatchers.IO).launch {
+                storageDataStore.storageLocation.set(storageLocation)
+            }
+        }
+    }
+
+    suspend fun setKeepIssueNumber(number: Int) {
+        storageDataStore.keepIssuesNumber.set(number)
     }
 
     fun setOnlyWifi(onlyWifi: Boolean) {
