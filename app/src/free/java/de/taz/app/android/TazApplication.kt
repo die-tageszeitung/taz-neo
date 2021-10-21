93b045bb4532cc0460535432b19776b7655d347e
Felix Höffken <felix@ctrl.alt.coop>
2021-10-21 16:29:17 +0200

    Create AbstractTazApplication

--- app/src/free/java/de/taz/app/android/TazApplication.kt
+++ app/src/free/java/de/taz/app/android/TazApplication.kt
@@ -1,33 +1,14 @@
 package de.taz.app.android
 
-import android.app.Application
-import android.os.StrictMode
-import com.facebook.stetho.Stetho
 import de.taz.app.android.download.DownloadService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 @Suppress("UNUSED")
-class TazApplication : Application() {
+class TazApplication : AbstractTazApplication() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Stetho.initializeWithDefaults(this)
-            StrictMode.setVmPolicy(
-                StrictMode.VmPolicy.Builder()
-                    .detectLeakedClosableObjects()
-                    .penaltyLog()
-                    .build()
-            )
-            StrictMode.setThreadPolicy(
-                StrictMode.ThreadPolicy.Builder()
-                    .detectDiskReads()
-                    .penaltyLog()
-                    .build()
-            )
-        }
-        SentryProvider.initSentry(this)
         DownloadService.createInstance(applicationContext).apply {
             CoroutineScope(Dispatchers.IO).launch {
                 scheduleNewestIssueDownload("poll/initial", true)
--- app/src/nonfree/java/de/taz/app/android/TazApplication.kt
+++ app/src/nonfree/java/de/taz/app/android/TazApplication.kt
@@ -9,23 +9,6 @@ import com.google.firebase.FirebaseApp
 class TazApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Stetho.initializeWithDefaults(this)
-            StrictMode.setVmPolicy(
-                StrictMode.VmPolicy.Builder()
-                    .detectLeakedClosableObjects()
-                    .penaltyLog()
-                    .build()
-            )
-            StrictMode.setThreadPolicy(
-                StrictMode.ThreadPolicy.Builder()
-                    .detectDiskReads()
-                    .penaltyLog()
-                    .build()
-            )
-        }
-        SentryProvider.initSentry(this)
         FirebaseApp.initializeApp(this)
-
     }
 }
