010b208bc6b2178b4b511e132eebbd080d5955bc
Jonatan Zint <jonatan@ctrl.alt.coop>
2021-10-26 22:09:42 +0200

    gotta have adapter decorators, otherwise release builds crash

--- app/src/main/java/de/taz/app/android/api/dto/SearchDto.kt
+++ app/src/main/java/de/taz/app/android/api/dto/SearchDto.kt
@@ -1,7 +1,9 @@
 package de.taz.app.android.api.dto
 
+import com.squareup.moshi.JsonClass
 import de.taz.app.android.api.models.AuthInfo
 
+@JsonClass(generateAdapter = true)
 data class SearchDto(
     val authInfo: AuthInfo,
     val sessionId: String,
