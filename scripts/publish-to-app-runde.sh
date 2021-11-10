0cee52f2f2ff468d469419fde9431e603d7240cf
eike <eike@ctrl.alt.coop>
2021-11-10 10:40:58 +0100

    Merge branch 'fix/removeRecipientFromPublishMail' into 'master'
    Remove recipient from post
    
    See merge request taz/taz-app!960

--- scripts/publish-to-app-runde.sh
+++ scripts/publish-to-app-runde.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 set -o xtrace
-RECIPIENT="app-runde@taz.de"
 
 
 
@@ -16,4 +15,4 @@ export DEBUG_APP_URL=`curl --header 'Accept: application/json' --header 'OCS-API
 export DEBUG_APP_VERSION=${CI_COMMIT_TAG}
 export DEBUG_APP_VERSION_CODE=${APP_VERSION_CODE:-`/opt/android/sdk/build-tools/29.0.2/aapt dump badging app/build/outputs/apk/freeTazProduction/debug/app-free-taz-production-debug.apk | grep "VersionCode" | sed -e "s/.*versionCode='//" -e "s/' .*//"`}
 
-http -v --ignore-stdin https://toolbox.alt.coop/misc/api/taz-release recipients:="[\"${RECIPIENT}\"]" debug_version_code="${DEBUG_APP_VERSION_CODE}" debug_version_name="${DEBUG_APP_VERSION}" debug_link=${DEBUG_APP_URL} free_version_code="${FREE_APP_VERSION_CODE}" free_version_name="${FREE_APP_VERSION}" free_link=${FREE_APP_URL} release_note="${RELEASE_MESSAGE}" Authorization:"Token ${TOOLBOX_API_TOKEN}"
+http -v --ignore-stdin https://toolbox.alt.coop/misc/api/taz-release staging_debug_link="${STAGING_DEBUG_APP_URL}" debug_link="${DEBUG_APP_URL}" free_link="${FREE_APP_URL}" version_code="${APP_VERSION_CODE}" version_name="${APP_VERSION}" release_note="${RELEASE_MESSAGE}" Authorization:"Token ${TOOLBOX_API_TOKEN}"
