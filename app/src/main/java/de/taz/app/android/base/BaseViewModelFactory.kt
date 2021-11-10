a12fb0c021b08208142336f73d04adba48706004
Felix Höffken <felix@ctrl.alt.coop>
2021-11-10 14:28:17 +0100

    Use sdk 31 and update dependencies

--- app/src/main/java/de/taz/app/android/base/BaseViewModelFactory.kt
+++ app/src/main/java/de/taz/app/android/base/BaseViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 
 class BaseViewModelFactory<T>(private val creator: () -> T) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return creator() as T
     }
 }
