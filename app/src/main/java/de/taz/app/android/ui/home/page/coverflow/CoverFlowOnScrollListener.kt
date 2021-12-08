0a48e3c57c400f5aac73e622ad8567aaabc2c153
Felix Höffken <felix@ctrl.alt.coop>
2021-12-08 21:03:39 +0100

    OnScrollListener only changes UI

--- app/src/main/java/de/taz/app/android/ui/home/page/coverflow/CoverFlowOnScrollListener.kt
+++ app/src/main/java/de/taz/app/android/ui/home/page/coverflow/CoverFlowOnScrollListener.kt
@@ -65,7 +65,7 @@ class CoverFlowOnScrollListener(
             }
         }
         (recyclerView.adapter as IssueFeedAdapter).getItem(position)?.let { date ->
-            fragment.skipToDate(date, scroll = false)
+            fragment.setDateTextAndDownloadObserver(date)
         }
     }
 
