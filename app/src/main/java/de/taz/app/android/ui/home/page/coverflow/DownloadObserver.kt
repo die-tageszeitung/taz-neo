5832b296c6238a656c58b789895461021ec1c314
eike <eike@ctrl.alt.coop>
2021-12-14 18:02:19 +0100

    Show popup on crousel

--- app/src/main/java/de/taz/app/android/ui/home/page/coverflow/DownloadObserver.kt
+++ app/src/main/java/de/taz/app/android/ui/home/page/coverflow/DownloadObserver.kt
@@ -1,13 +1,20 @@
 package de.taz.app.android.ui.home.page.coverflow
 
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import de.taz.app.android.R
 import de.taz.app.android.content.ContentService
 import de.taz.app.android.content.cache.*
+import de.taz.app.android.dataStore.DownloadDataStore
 import de.taz.app.android.persistence.repository.AbstractIssuePublication
+import de.taz.app.android.persistence.repository.IssuePublication
+import de.taz.app.android.persistence.repository.IssuePublicationWithPages
 import de.taz.app.android.singletons.ToastHelper
 import de.taz.app.android.ui.cover.MOMENT_FADE_DURATION_MS
 import de.taz.app.android.util.IssuePublicationMonitor
@@ -44,6 +51,8 @@ class DownloadObserver(
         fragment.requireContext().applicationContext,
         issuePublication
     ).issueCacheLiveData
+
+    private val downloadDataStore = DownloadDataStore.getInstance(fragment.requireContext().applicationContext)
 
     fun startObserving() {
         hideDownloadIcon()
@@ -90,7 +99,7 @@ class DownloadObserver(
         downloadIconView.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 try {
-                    contentService.downloadToCache(issuePublication)
+                    showAutomaticDownloadDialog()
                 } catch (e: CacheOperationFailedException) {
                     withContext(Dispatchers.Main) {
                         fragment.requireActivity().showIssueDownloadFailedDialog(
@@ -128,5 +137,55 @@ class DownloadObserver(
         downloadIconView.visibility = View.GONE
         checkmarkIconView.visibility = View.GONE
         downloadProgressView.visibility = View.VISIBLE
+    }
+
+    private suspend fun showAutomaticDownloadDialog() {
+        val showDialog = !downloadDataStore.pdfDialogDoNotShowAgain.get()
+                && issuePublication is IssuePublication
+
+        if (showDialog) {
+            withContext(Dispatchers.Main) {
+                val dialogView = LayoutInflater.from(fragment.context)
+                    .inflate(R.layout.dialog_settings_download_pdf, null)
+                val doNotShowAgainCheckboxView =
+                    dialogView?.findViewById<MaterialCheckBox>(R.id.dialog_settings_download_pdf_do_not_ask_again)
+                val dialog = MaterialAlertDialogBuilder(fragment.requireContext())
+                    .setView(dialogView)
+                    .setNegativeButton(R.string.settings_dialog_download_too_much_data) { dialog, _ ->
+                        setPdfSwitch(
+                            doNotShowAgainCheckboxView,
+                            issuePublication,
+                            enabled = false
+                        )
+                        dialog.dismiss()
+                    }
+                    .setPositiveButton(R.string.settings_dialog_download_load_pdf) { dialog, _ ->
+                        setPdfSwitch(
+                            doNotShowAgainCheckboxView,
+                            IssuePublicationWithPages(issuePublication),
+                            enabled = true
+                        )
+                        dialog.dismiss()
+                    }
+                    .create()
+                dialog.show()
+            }
+        } else {
+            contentService.downloadToCache(issuePublication)
+        }
+    }
+
+    private fun setPdfSwitch(
+        doNotShowAgainCheckboxView: MaterialCheckBox?,
+        issuePublication: AbstractIssuePublication,
+        enabled: Boolean
+    ) {
+        CoroutineScope(Dispatchers.IO).launch {
+            contentService.downloadToCache(issuePublication)
+            downloadDataStore.pdfAdditionally.set(enabled)
+            downloadDataStore.pdfDialogDoNotShowAgain.set(
+                doNotShowAgainCheckboxView?.isChecked == true
+            )
+        }
     }
 }
