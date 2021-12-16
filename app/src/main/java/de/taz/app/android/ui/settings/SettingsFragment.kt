398127aafc4ae6b6c3a0c64061ec3df5fbb7f5e1
eike <eike@ctrl.alt.coop>
2021-12-16 15:34:02 +0100

    Remove dialog from settings

--- app/src/main/java/de/taz/app/android/ui/settings/SettingsFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/settings/SettingsFragment.kt
@@ -167,22 +167,8 @@ class SettingsFragment : BaseViewModelFragment<SettingsViewModel>(R.layout.fragm
                 setDownloadEnabled(isChecked)
             }
 
-            fragment_settings_auto_download_switch?.setOnClickListener {
-                if (viewModel.downloadAutomaticallyLiveData.value == false
-                    && viewModel.downloadAdditionallyDialogDoNotShowAgain.value != true
-                ) {
-                    showAutomaticDownloadDialog()
-                }
-            }
-
             fragment_settings_auto_pdf_download_switch?.setOnCheckedChangeListener { _, isChecked ->
                 setPdfDownloadEnabled(isChecked)
-            }
-
-            fragment_settings_auto_pdf_download_switch?.setOnClickListener {
-                if (viewModel.downloadAutomaticallyLiveData.value == true) {
-                    showAutomaticDownloadDialog()
-                }
             }
 
             fragment_settings_delete_all_issues.setOnClickListener {
@@ -218,9 +204,6 @@ class SettingsFragment : BaseViewModelFragment<SettingsViewModel>(R.layout.fragm
             downloadAdditionallyPdf.observeDistinct(viewLifecycleOwner) { additionallyEnabled ->
                 showDownloadAdditionallyPdf(additionallyEnabled)
             }
-            downloadAdditionallyDialogDoNotShowAgain.observeDistinct(viewLifecycleOwner) { doNotShowAgain ->
-                setDoNotShowAgain(doNotShowAgain)
-            }
             storageLocationLiveData.observeDistinct(viewLifecycleOwner) { storageLocation ->
                 if (lastStorageLocation != null && lastStorageLocation != storageLocation) {
                     toastHelper.showToast(R.string.settings_storage_migration_hint)
@@ -253,37 +236,6 @@ class SettingsFragment : BaseViewModelFragment<SettingsViewModel>(R.layout.fragm
 
     private fun setDoNotShowAgain(doNotShowAgain: Boolean) {
         viewModel.setPdfDialogDoNotShowAgain(doNotShowAgain)
-    }
-
-    private fun showAutomaticDownloadDialog() {
-        val dialogView = LayoutInflater.from(context)
-            .inflate(R.layout.dialog_settings_download_pdf, null)
-        val doNotShowAgainCheckboxView = dialogView?.findViewById<MaterialCheckBox>(R.id.dialog_settings_download_pdf_do_not_ask_again)
-        val pdfSwitchView = view?.findViewById<SwitchCompat>(R.id.fragment_settings_auto_pdf_download_switch)
-        val dialog = MaterialAlertDialogBuilder(requireContext())
-            .setView(dialogView)
-            .setNegativeButton(R.string.settings_dialog_download_too_much_data) { dialog, _ ->
-                setPdfSwitch(pdfSwitchView, doNotShowAgainCheckboxView, enabled = false)
-                dialog.dismiss()
-            }
-            .setPositiveButton(R.string.settings_dialog_download_load_pdf) { dialog, _ ->
-                setPdfSwitch(pdfSwitchView, doNotShowAgainCheckboxView, enabled = true)
-                dialog.dismiss()
-            }
-            .create()
-        dialog.show()
-    }
-
-    private fun setPdfSwitch(
-        pdfSwitchView: SwitchCompat?,
-        doNotShowAgainCheckboxView: MaterialCheckBox?,
-        enabled: Boolean
-    ) {
-        if (doNotShowAgainCheckboxView?.isChecked == true) {
-            setDoNotShowAgain(true)
-        }
-        pdfSwitchView?.isChecked = enabled
-        setPdfDownloadEnabled(enabled)
     }
 
     private fun showKeepIssuesDialog() {
@@ -428,9 +380,6 @@ class SettingsFragment : BaseViewModelFragment<SettingsViewModel>(R.layout.fragm
         view?.findViewById<SwitchCompat>(R.id.fragment_settings_auto_download_switch)?.isChecked =
             downloadsEnabled
         view?.findViewById<SwitchCompat>(R.id.fragment_settings_auto_download_wifi_switch)?.apply {
-            isEnabled = downloadsEnabled
-        }
-        view?.findViewById<SwitchCompat>(R.id.fragment_settings_auto_pdf_download_switch)?.apply {
             isEnabled = downloadsEnabled
         }
     }
