30b23d33fa57917d242c1f933c93bbdc779f293f
eike <eike@ctrl.alt.coop>
2021-12-16 16:24:53 +0100

    Linting LiveData

--- app/src/main/java/de/taz/app/android/util/LiveData.kt
+++ app/src/main/java/de/taz/app/android/util/LiveData.kt
@@ -1,14 +1,11 @@
 package de.taz.app.android.util
 
 import android.content.Context
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.asLiveData
 import de.taz.app.android.content.ContentService
 import de.taz.app.android.content.cache.CacheStateUpdate
 import de.taz.app.android.persistence.repository.AbstractIssuePublication
-import de.taz.app.android.persistence.repository.IssueKey
-import de.taz.app.android.persistence.repository.IssueRepository
 import de.taz.app.android.singletons.AuthHelper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -24,7 +21,7 @@ class IssuePublicationMonitor(
 
     val issueCacheLiveData: MediatorLiveData<CacheStateUpdate> =
         MediatorLiveData<CacheStateUpdate>().apply {
-            addSource(authHelper.minStatusLiveData) { minStatus ->
+            addSource(authHelper.minStatusLiveData) {
                 CoroutineScope(Dispatchers.Default).launch {
                     postValue(
                         contentService.getCacheState(publication)
