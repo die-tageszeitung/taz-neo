99d12584207cd19e27dc9cdb5f8f995da5d6d455
Felix Höffken <felix@ctrl.alt.coop>
2022-01-06 17:27:28 +0100

    Use ViewBindings in DataPolicyActivity

--- app/src/main/java/de/taz/app/android/ui/DataPolicyActivity.kt
+++ app/src/main/java/de/taz/app/android/ui/DataPolicyActivity.kt
@@ -6,15 +6,15 @@ import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import de.taz.app.android.LOADING_SCREEN_FADE_OUT_TIME
-import de.taz.app.android.R
 import de.taz.app.android.api.models.ResourceInfoKey
+import de.taz.app.android.base.ViewBindingActivity
 import de.taz.app.android.content.ContentService
 import de.taz.app.android.content.cache.CacheOperationFailedException
 import de.taz.app.android.data.DataService
 import de.taz.app.android.dataStore.GeneralDataStore
+import de.taz.app.android.databinding.ActivityDataPolicyBinding
 import de.taz.app.android.persistence.repository.FileEntryRepository
 import de.taz.app.android.singletons.StorageService
 import de.taz.app.android.ui.main.MainActivity
@@ -23,12 +23,11 @@ import de.taz.app.android.util.Log
 import de.taz.app.android.util.showConnectionErrorDialog
 import de.taz.app.android.util.showFatalErrorDialog
 import io.sentry.Sentry
-import kotlinx.android.synthetic.main.activity_data_policy.*
 import kotlinx.coroutines.*
 
 const val FINISH_ON_CLOSE = "FINISH ON CLOSE"
 
-class DataPolicyActivity : AppCompatActivity() {
+class DataPolicyActivity : ViewBindingActivity<ActivityDataPolicyBinding>() {
 
     private val log by Log
     private val dataPolicyPageName = "welcomeSlidesDataPolicy.html"
@@ -53,43 +52,42 @@ class DataPolicyActivity : AppCompatActivity() {
         fileEntryRepository = FileEntryRepository.getInstance(applicationContext)
         contentService = ContentService.getInstance(applicationContext)
 
-        setContentView(R.layout.activity_data_policy)
-
-        data_policy_accept_button?.setOnClickListener {
-            acceptDataPolicy()
-            if (finishOnClose) {
-                finish()
-            } else {
-                if (hasSeenWelcomeScreen()) {
-                    log.debug("start welcome activity")
-                    val intent = Intent(applicationContext, WelcomeActivity::class.java)
-                    intent.putExtra(START_HOME_ACTIVITY, true)
-                    intent.flags =
-                        Intent.FLAG_ACTIVITY_NO_ANIMATION or
-                                Intent.FLAG_ACTIVITY_NEW_TASK or
-                                Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    startActivity(Intent(intent))
+        viewBinding.apply {
+            dataPolicyAcceptButton.setOnClickListener {
+                acceptDataPolicy()
+                if (finishOnClose) {
+                    finish()
                 } else {
-                    log.debug("start main activity")
-                    MainActivity.start(applicationContext, Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                    if (hasSeenWelcomeScreen()) {
+                        log.debug("start welcome activity")
+                        val intent = Intent(applicationContext, WelcomeActivity::class.java)
+                        intent.putExtra(START_HOME_ACTIVITY, true)
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_NO_ANIMATION or
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or
+                                    Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(Intent(intent))
+                    } else {
+                        log.debug("start main activity")
+                        MainActivity.start(applicationContext, Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                    }
                 }
             }
-        }
 
-        data_policy_fullscreen_content.apply {
-            webViewClient = object : WebViewClient() {
-                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                    startActivity(intent)
-                    return true
+            dataPolicyFullscreenContent.apply {
+                webViewClient = object : WebViewClient() {
+                    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                        startActivity(intent)
+                        return true
+                    }
+                }
+                webChromeClient = AppWebChromeClient(::hideLoadingScreen)
+
+                lifecycleScope.launch(Dispatchers.IO) {
+                    ensureResourceInfoIsDownloadedAndShowDataPolicy()
                 }
             }
-            webChromeClient = AppWebChromeClient(::hideLoadingScreen)
-
-            lifecycleScope.launch(Dispatchers.IO) {
-                ensureResourceInfoIsDownloadedAndShowDataPolicy()
-            }
-
         }
     }
 
@@ -103,27 +101,28 @@ class DataPolicyActivity : AppCompatActivity() {
         !generalDataStore.hasSeenWelcomeScreen.get()
     }
 
-    private suspend fun ensureResourceInfoIsDownloadedAndShowDataPolicy() = withContext(Dispatchers.Main) {
-        try {
-            contentService.downloadToCache(ResourceInfoKey(-1))
-            showDataPolicy()
-        } catch (e: CacheOperationFailedException) {
-            showConnectionErrorDialog()
-            Sentry.captureException(e)
-        } catch (e: HTMLFileNotFoundException) {
-            val hint = "Html file for data policy not found"
-            log.error(hint)
-            Sentry.captureException(e, hint)
-            showFatalErrorDialog()
+    private suspend fun ensureResourceInfoIsDownloadedAndShowDataPolicy() =
+        withContext(Dispatchers.Main) {
+            try {
+                contentService.downloadToCache(ResourceInfoKey(-1))
+                showDataPolicy()
+            } catch (e: CacheOperationFailedException) {
+                showConnectionErrorDialog()
+                Sentry.captureException(e)
+            } catch (e: HTMLFileNotFoundException) {
+                val hint = "Html file for data policy not found"
+                log.error(hint)
+                Sentry.captureException(e, hint)
+                showFatalErrorDialog()
+            }
         }
-    }
 
     private suspend fun showDataPolicy() = withContext(Dispatchers.IO) {
         fileEntryRepository.get(dataPolicyPageName)?.let {
             storageService.getFileUri(it)
         }?.let {
             withContext(Dispatchers.Main) {
-                data_policy_fullscreen_content.loadUrl(it)
+                viewBinding.dataPolicyFullscreenContent.loadUrl(it)
             }
         } ?: run {
             throw HTMLFileNotFoundException("Data policy html file ($dataPolicyPageName) not found in database")
@@ -132,12 +131,13 @@ class DataPolicyActivity : AppCompatActivity() {
 
     private fun hideLoadingScreen() {
         this.runOnUiThread {
-            data_policy_loading_screen?.animate()?.apply {
-                alpha(0f)
-                duration = LOADING_SCREEN_FADE_OUT_TIME
-                withEndAction {
-                    data_policy_loading_screen?.visibility = View.GONE
-                }
+            viewBinding.dataPolicyLoadingScreen.root.apply {
+                animate()
+                    .alpha(0f)
+                    .withEndAction {
+                        visibility = View.GONE
+                    }
+                    .duration = LOADING_SCREEN_FADE_OUT_TIME
             }
         }
     }
