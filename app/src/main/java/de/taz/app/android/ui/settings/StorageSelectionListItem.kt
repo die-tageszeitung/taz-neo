ba1be3596e9a666040857f9d614c57fc0e041c47
Felix Höffken <felix@ctrl.alt.coop>
2022-01-06 18:24:59 +0100

    Remove synthetic imports from StorageSelectionListItem

--- app/src/main/java/de/taz/app/android/ui/settings/StorageSelectionListItem.kt
+++ app/src/main/java/de/taz/app/android/ui/settings/StorageSelectionListItem.kt
@@ -3,16 +3,18 @@ package de.taz.app.android.ui.settings
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.Checkable
+import android.widget.RadioButton
 import androidx.constraintlayout.widget.ConstraintLayout
 import de.taz.app.android.R
-import kotlinx.android.synthetic.main.listitem_select_storage.view.*
 
 class StorageSelectionListItem @JvmOverloads constructor(
     context: Context, attributeSet: AttributeSet? = null, defStyle: Int = 0
 ) : ConstraintLayout(context, attributeSet, defStyle), Checkable {
+    private val itemCheckbox: RadioButton
     init {
         this.descendantFocusability = FOCUS_BLOCK_DESCENDANTS
-        inflate(context, R.layout.listitem_select_storage, this)
+        val view = inflate(context, R.layout.listitem_select_storage, this)
+        itemCheckbox = view.findViewById(R.id.itemCheckbox)
     }
 
     override fun setChecked(checked: Boolean) {
