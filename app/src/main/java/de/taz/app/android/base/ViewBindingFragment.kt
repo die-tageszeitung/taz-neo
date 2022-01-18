c165c41d35a11841be215f37fd0ae47ecea71642
Felix Höffken <felix@ctrl.alt.coop>
2022-01-18 20:19:01 +0100

    Add ViewBindingFragment and use it in ArticleLoginFragment

---
+++ app/src/main/java/de/taz/app/android/base/ViewBindingFragment.kt
@@ -0,0 +1,56 @@
+package de.taz.app.android.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import java.lang.reflect.ParameterizedType
+
+abstract class ViewBindingFragment<ViewBindingClass : ViewBinding> : Fragment() {
+
+    private var _binding: ViewBindingClass? = null
+
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    protected val viewBinding get() = _binding!!
+
+    protected val rootView: View get() = viewBinding.root
+    protected val rootViewGroup get() = rootView as? ViewGroup
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = createBinding(inflater, container)
+        val view = rootView
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun createBinding(layoutInflater: LayoutInflater, container: ViewGroup?): ViewBindingClass {
+        val viewBindingClass =
+            try {
+                (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0]
+            } catch (cce: ClassCastException) {
+                ((javaClass.genericSuperclass as Class<ViewBindingClass>)
+                    .genericSuperclass as ParameterizedType).actualTypeArguments[0]
+            } as Class<ViewBindingClass>
+        val method = viewBindingClass.getMethod(
+            "inflate",
+            LayoutInflater::class.java,
+            ViewGroup::class.java,
+            Boolean::class.java
+        )
+        return method.invoke(this, layoutInflater, container, false) as ViewBindingClass
+    }
+
+}
+
