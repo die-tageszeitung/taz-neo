6d2fa01eba13acad7c23bc1f826ca522b7e52630
Felix Höffken <felix@ctrl.alt.coop>
2022-05-09 16:48:50 +0200

    Merge branch 'fix/doNotDeleteWhileReading2/14105' into 'master'
    Exclude last read issue from deletion
    
    See merge request taz/taz-app!1086

--- app/src/main/java/de/taz/app/android/ui/settings/SettingsFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/settings/SettingsFragment.kt
@@ -243,7 +243,7 @@ class SettingsFragment : BaseViewModelFragment<SettingsViewModel, FragmentSettin
                     (dialog as AlertDialog).findViewById<EditText>(
                         R.id.dialog_settings_keep_number
                     )?.text.toString().toIntOrNull()?.let { number ->
-                        setStoredIssueNumber(number.coerceAtLeast(1))
+                        setStoredIssueNumber(number)
                         dialog.hide()
                     }
                 }
