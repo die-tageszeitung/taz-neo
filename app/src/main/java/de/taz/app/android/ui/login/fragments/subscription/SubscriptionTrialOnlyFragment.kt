4472ac0ebb28c2688b77746f0dcd4c4dbc1412b0
eike <eike@ctrl.alt.coop>
2022-05-12 17:27:46 +0200

    Merge branch 'feat/versionWithoutAppPayments' into 'master'
    Feat/version without app payments
    
    See merge request taz/taz-app!1089

---
+++ app/src/main/java/de/taz/app/android/ui/login/fragments/subscription/SubscriptionTrialOnlyFragment.kt
@@ -0,0 +1,43 @@
+package de.taz.app.android.ui.login.fragments.subscription
+
+import android.os.Bundle
+import android.view.View
+import de.taz.app.android.R
+import de.taz.app.android.ui.login.LoginViewModelState
+import kotlinx.android.synthetic.main.fragment_subscription_trial_only.*
+
+
+class SubscriptionTrialOnlyFragment : SubscriptionBaseFragment(R.layout.fragment_subscription_trial_only) {
+    private var elapsed: Boolean = false
+    companion object {
+        fun createInstance(
+            elapsed: Boolean
+        ): SubscriptionTrialOnlyFragment {
+            val fragment = SubscriptionTrialOnlyFragment()
+            fragment.elapsed = elapsed
+            return fragment
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (elapsed) {
+            fragment_subscription_trial_only_description.visibility = View.GONE
+            fragment_subscription_trial_only_description_elapsed.visibility = View.VISIBLE
+            fragment_subscription_address_proceed.text =
+                getString(R.string.close_okay)
+            fragment_subscription_address_proceed.setOnClickListener { this.activity?.finish() }
+        } else {
+            fragment_subscription_address_proceed.setOnClickListener { ifDoneNext() }
+        }
+    }
+
+    override fun done(): Boolean {
+        viewModel.price = 0
+        return true
+    }
+
+    override fun next() {
+        viewModel.status.postValue(LoginViewModelState.SUBSCRIPTION_ADDRESS)
+    }
+}
