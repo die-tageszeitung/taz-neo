c2115b209d258f96d86e6bc906543eebfbe5c9e3
eike <eike@ctrl.alt.coop>
2022-05-30 11:19:01 +0200

    Use lifecyclescope to kill coroutines when fragment is destroyed
    Additionally use whenResumed to ensure that the UI is there

--- app/src/main/java/de/taz/app/android/ui/webview/WebViewFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/webview/WebViewFragment.kt
@@ -10,6 +10,7 @@ import android.widget.LinearLayout
 import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenResumed
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.appbar.AppBarLayout
 import de.taz.app.android.LOADING_SCREEN_FADE_OUT_TIME
@@ -65,10 +66,12 @@ abstract class WebViewFragment<
         get() = viewBinding.root.findViewById(R.id.web_view)
 
     private fun reloadAfterCssChange() {
-        CoroutineScope(Dispatchers.Main).launch {
-            webView.injectCss()
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.N)
-                webView.reload()
+        CoroutineScope(Dispatchers.Main + lifecycleScope.coroutineContext).launch {
+            whenResumed {
+                webView.injectCss()
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.N)
+                    webView.reload()
+            }
         }
     }
 
