8a9715a4622aa19d0ab109e71d044197d01209ec
eike <eike@ctrl.alt.coop>
2022-06-09 16:44:34 +0200

    Use correct default image

--- app/src/main/java/de/taz/app/android/Constants.kt
+++ app/src/main/java/de/taz/app/android/Constants.kt
@@ -9,7 +9,7 @@ const val DEBUG_VERSION_DOWNLOAD_ENDPOINT = "https://dl.taz.de/down/taz.neo.debu
 
 // UI
 const val DEFAULT_MOMENT_RATIO = "0.670219:1"
-const val DEFAULT_MOMENT_FILE = "navButton.taz.normal.png"
+const val DEFAULT_MOMENT_FILE = "noInternetMoment.png"
 const val DEFAULT_NAV_DRAWER_FILE_NAME = "navButton.taz.normal.png"
 const val SUBSCRIPTION_EMAIL_ADDRESS = "digiabo@taz.de"
 const val WEBVIEW_DRAG_SENSITIVITY_FACTOR = 2
