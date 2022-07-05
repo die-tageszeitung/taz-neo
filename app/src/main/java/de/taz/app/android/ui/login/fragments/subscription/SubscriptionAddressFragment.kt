b9bc243488f5ced84f5b2c3a8d39b3bc86da0274
Felix Höffken <felix@ctrl.alt.coop>
2022-07-05 15:45:15 +0200

    Remove synthetic imports

--- app/src/main/java/de/taz/app/android/ui/login/fragments/subscription/SubscriptionAddressFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/login/fragments/subscription/SubscriptionAddressFragment.kt
@@ -7,16 +7,16 @@ import androidx.annotation.StringRes
 import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
 import de.taz.app.android.R
+import de.taz.app.android.databinding.FragmentSubscriptionAddressBinding
 import de.taz.app.android.listener.OnEditorActionDoneListener
 import de.taz.app.android.monkey.markRequired
 import de.taz.app.android.monkey.setError
 import de.taz.app.android.ui.login.LoginViewModelState
-import kotlinx.android.synthetic.main.fragment_subscription_address.*
 
 const val MAX_NAME_LENGTH = 24
 
 class SubscriptionAddressFragment :
-    SubscriptionBaseFragment(R.layout.fragment_subscription_address) {
+    SubscriptionBaseFragment<FragmentSubscriptionAddressBinding>() {
     var cityInvalid: Boolean = false
     var countryInvalid: Boolean = false
     var postcodeInvalid: Boolean = false
@@ -56,129 +56,133 @@ class SubscriptionAddressFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        fragment_subscription_address_first_name_layout.markRequired()
-        fragment_subscription_address_surname_layout.markRequired()
-        fragment_subscription_address_street_layout.markRequired()
-        fragment_subscription_address_city_layout.markRequired()
-        fragment_subscription_address_country_layout.markRequired()
-        fragment_subscription_address_postcode_layout.markRequired()
+        viewBinding.apply {
+            fragmentSubscriptionAddressFirstNameLayout.markRequired()
+            fragmentSubscriptionAddressSurnameLayout.markRequired()
+            fragmentSubscriptionAddressStreetLayout.markRequired()
+            fragmentSubscriptionAddressCityLayout.markRequired()
+            fragmentSubscriptionAddressCountryLayout.markRequired()
+            fragmentSubscriptionAddressPostcodeLayout.markRequired()
 
-        if (viewModel.price == 0) {
-            fragment_subscription_address_street_layout.visibility = View.GONE
-            fragment_subscription_address_city_layout.visibility = View.GONE
-            fragment_subscription_address_postcode_layout.visibility = View.GONE
-            fragment_subscription_address_country_layout.visibility = View.GONE
-            fragment_subscription_address_phone_layout.visibility = View.GONE
-            fragment_subscription_address_name_affix.imeOptions = EditorInfo.IME_ACTION_DONE
-        }
+            if (viewModel.price == 0) {
+                fragmentSubscriptionAddressStreetLayout.visibility = View.GONE
+                fragmentSubscriptionAddressCityLayout.visibility = View.GONE
+                fragmentSubscriptionAddressPostcodeLayout.visibility = View.GONE
+                fragmentSubscriptionAddressCountryLayout.visibility = View.GONE
+                fragmentSubscriptionAddressPhoneLayout.visibility = View.GONE
+                fragmentSubscriptionAddressNameAffix.imeOptions = EditorInfo.IME_ACTION_DONE
+            }
 
-        fragment_subscription_address_first_name.setText(viewModel.firstName)
-        fragment_subscription_address_surname.setText(viewModel.surName)
-        fragment_subscription_address_street.setText(viewModel.street)
-        fragment_subscription_address_city.setText(viewModel.city)
-        fragment_subscription_address_country.setText(viewModel.country)
-        fragment_subscription_address_postcode.setText(viewModel.postCode)
+            fragmentSubscriptionAddressFirstName.setText(viewModel.firstName)
+            fragmentSubscriptionAddressSurname.setText(viewModel.surName)
+            fragmentSubscriptionAddressStreet.setText(viewModel.street)
+            fragmentSubscriptionAddressCity.setText(viewModel.city)
+            fragmentSubscriptionAddressCountry.setText(viewModel.country)
+            fragmentSubscriptionAddressPostcode.setText(viewModel.postCode)
 
-        fragment_subscription_address_first_name.doAfterTextChanged { text ->
-            fragment_subscription_address_surname_layout.counterMaxLength =
-                (MAX_NAME_LENGTH - (text?.length ?: 0)).coerceIn(1, MAX_NAME_LENGTH - 1)
-        }
+            fragmentSubscriptionAddressFirstName.doAfterTextChanged { text ->
+                fragmentSubscriptionAddressSurnameLayout.counterMaxLength =
+                    (MAX_NAME_LENGTH - (text?.length ?: 0)).coerceIn(1, MAX_NAME_LENGTH - 1)
+            }
 
-        fragment_subscription_address_surname.doAfterTextChanged { text ->
-            fragment_subscription_address_first_name_layout.counterMaxLength =
-                (MAX_NAME_LENGTH - (text?.length ?: 0)).coerceIn(1, MAX_NAME_LENGTH - 1)
-        }
+            fragmentSubscriptionAddressSurname.doAfterTextChanged { text ->
+                fragmentSubscriptionAddressFirstNameLayout.counterMaxLength =
+                    (MAX_NAME_LENGTH - (text?.length ?: 0)).coerceIn(1, MAX_NAME_LENGTH - 1)
+            }
 
-        fragment_subscription_address_phone.setOnEditorActionListener(
-            OnEditorActionDoneListener(::ifDoneNext)
-        )
+            fragmentSubscriptionAddressPhone.setOnEditorActionListener(
+                OnEditorActionDoneListener(::ifDoneNext)
+            )
 
-        fragment_subscription_address_name_affix.setOnEditorActionListener(
-            OnEditorActionDoneListener(::ifDoneNext)
-        )
+            fragmentSubscriptionAddressNameAffix.setOnEditorActionListener(
+                OnEditorActionDoneListener(::ifDoneNext)
+            )
 
-        fragment_subscription_address_proceed.setOnClickListener {
-            ifDoneNext()
-        }
+            fragmentSubscriptionAddressProceed.setOnClickListener {
+                ifDoneNext()
+            }
 
-        if (nameTooLong) {
-            setFirstNameError(R.string.login_first_name_helper)
-            setSurnameError(R.string.login_surname_helper)
-        }
+            if (nameTooLong) {
+                setFirstNameError(R.string.login_first_name_helper)
+                setSurnameError(R.string.login_surname_helper)
+            }
 
-        if (firstNameEmpty) {
-            setFirstNameError(R.string.login_first_name_error_empty)
-        }
-        if (firstNameInvalid) {
-            setFirstNameError(R.string.login_first_name_error_invalid)
-        }
-        if (surnameEmpty) {
-            setSurnameError(R.string.login_surname_error_empty)
-        }
-        if (surnameInvalid) {
-            setSurnameError(R.string.login_surname_error_invalid)
-        }
+            if (firstNameEmpty) {
+                setFirstNameError(R.string.login_first_name_error_empty)
+            }
+            if (firstNameInvalid) {
+                setFirstNameError(R.string.login_first_name_error_invalid)
+            }
+            if (surnameEmpty) {
+                setSurnameError(R.string.login_surname_error_empty)
+            }
+            if (surnameInvalid) {
+                setSurnameError(R.string.login_surname_error_invalid)
+            }
 
-        if (cityInvalid) {
-            fragment_subscription_address_city_layout.setError(R.string.subscription_field_invalid)
-        }
-        if (countryInvalid) {
-            fragment_subscription_address_country_layout.setError(R.string.subscription_field_invalid)
-        }
-        if (streetInvalid) {
-            fragment_subscription_address_street_layout.setError(R.string.subscription_field_invalid)
-        }
-        if (postcodeInvalid) {
-            fragment_subscription_address_postcode_layout.setError(R.string.subscription_field_invalid)
+            if (cityInvalid) {
+                fragmentSubscriptionAddressCityLayout.setError(R.string.subscription_field_invalid)
+            }
+            if (countryInvalid) {
+                fragmentSubscriptionAddressCountryLayout.setError(R.string.subscription_field_invalid)
+            }
+            if (streetInvalid) {
+                fragmentSubscriptionAddressStreetLayout.setError(R.string.subscription_field_invalid)
+            }
+            if (postcodeInvalid) {
+                fragmentSubscriptionAddressPostcodeLayout.setError(R.string.subscription_field_invalid)
+            }
         }
     }
 
     override fun done(): Boolean {
         var done = true
-        if (fragment_subscription_address_first_name.text.isNullOrBlank()) {
-            setFirstNameError(R.string.login_first_name_error_empty)
-            done = false
-        }
-        if (fragment_subscription_address_surname.text.isNullOrBlank()) {
-            setSurnameError(R.string.login_surname_error_empty)
-            done = false
-        }
-        if (fragment_subscription_address_street_layout.isVisible
-            && fragment_subscription_address_street.text.isNullOrBlank()
-        ) {
-            fragment_subscription_address_street_layout.error =
-                context?.getString(R.string.street_error_empty)
-            done = false
-        }
-        if (fragment_subscription_address_city_layout.isVisible
-            && fragment_subscription_address_city.text.isNullOrBlank()
-        ) {
-            fragment_subscription_address_city_layout.error =
-                context?.getString(R.string.city_error_empty)
-            done = false
-        }
-        if (fragment_subscription_address_country_layout.isVisible
-            && fragment_subscription_address_country.text.isNullOrBlank()
-        ) {
-            fragment_subscription_address_country_layout.error =
-                context?.getString(R.string.country_error_empty)
-            done = false
-        }
-        if (fragment_subscription_address_postcode_layout.isVisible
-            && fragment_subscription_address_postcode.text.isNullOrBlank()
-        ) {
-            fragment_subscription_address_postcode_layout.error =
-                context?.getString(R.string.postcode_error_empty)
-            done = false
-        }
-        viewModel.apply {
-            firstName = fragment_subscription_address_first_name.text.toString()
-            surName = fragment_subscription_address_surname.text.toString()
-            street = fragment_subscription_address_street.text.toString()
-            city = fragment_subscription_address_city.text.toString()
-            country = fragment_subscription_address_country.text.toString()
-            postCode = fragment_subscription_address_postcode.text.toString()
-            phone = fragment_subscription_address_phone.text.toString()
+        viewBinding.apply {
+            if (fragmentSubscriptionAddressFirstName.text.isNullOrBlank()) {
+                setFirstNameError(R.string.login_first_name_error_empty)
+                done = false
+            }
+            if (fragmentSubscriptionAddressSurname.text.isNullOrBlank()) {
+                setSurnameError(R.string.login_surname_error_empty)
+                done = false
+            }
+            if (fragmentSubscriptionAddressStreetLayout.isVisible
+                && fragmentSubscriptionAddressStreet.text.isNullOrBlank()
+            ) {
+                fragmentSubscriptionAddressStreetLayout.error =
+                    context?.getString(R.string.street_error_empty)
+                done = false
+            }
+            if (fragmentSubscriptionAddressCityLayout.isVisible
+                && fragmentSubscriptionAddressCity.text.isNullOrBlank()
+            ) {
+                fragmentSubscriptionAddressCityLayout.error =
+                    context?.getString(R.string.city_error_empty)
+                done = false
+            }
+            if (fragmentSubscriptionAddressCountryLayout.isVisible
+                && fragmentSubscriptionAddressCountry.text.isNullOrBlank()
+            ) {
+                fragmentSubscriptionAddressCountryLayout.error =
+                    context?.getString(R.string.country_error_empty)
+                done = false
+            }
+            if (fragmentSubscriptionAddressPostcodeLayout.isVisible
+                && fragmentSubscriptionAddressPostcode.text.isNullOrBlank()
+            ) {
+                fragmentSubscriptionAddressPostcodeLayout.error =
+                    context?.getString(R.string.postcode_error_empty)
+                done = false
+            }
+            viewModel.apply {
+                firstName = fragmentSubscriptionAddressFirstName.text.toString()
+                surName = fragmentSubscriptionAddressSurname.text.toString()
+                street = fragmentSubscriptionAddressStreet.text.toString()
+                city = fragmentSubscriptionAddressCity.text.toString()
+                country = fragmentSubscriptionAddressCountry.text.toString()
+                postCode = fragmentSubscriptionAddressPostcode.text.toString()
+                phone = fragmentSubscriptionAddressPhone.text.toString()
+            }
         }
         return done
     }
@@ -192,11 +196,11 @@ class SubscriptionAddressFragment :
         }
     }
 
-    fun setFirstNameError(@StringRes stringRes: Int) {
-        fragment_subscription_address_first_name_layout.setError(stringRes)
+    private fun setFirstNameError(@StringRes stringRes: Int) {
+        viewBinding.fragmentSubscriptionAddressFirstNameLayout.setError(stringRes)
     }
 
-    fun setSurnameError(@StringRes stringRes: Int) {
-        fragment_subscription_address_surname_layout.setError(stringRes)
+    private fun setSurnameError(@StringRes stringRes: Int) {
+        viewBinding.fragmentSubscriptionAddressSurnameLayout.setError(stringRes)
     }
 }
