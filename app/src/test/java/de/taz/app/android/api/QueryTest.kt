32e66c9937bd8e9c8d8e582b0aea3aff8c71cd45
Felix Höffken <felix@ctrl.alt.coop>
2022-07-19 18:21:16 +0200

    Make tests pass

--- app/src/test/java/de/taz/app/android/api/QueryTest.kt
+++ app/src/test/java/de/taz/app/android/api/QueryTest.kt
@@ -1,7 +1,9 @@
 package de.taz.app.android.api
 
 import de.taz.app.android.api.dto.DeviceFormat
+import de.taz.app.android.api.dto.DeviceType
 import de.taz.app.android.api.variables.AuthenticationVariables
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.decodeFromString
 import org.junit.Assert.assertEquals
@@ -34,12 +36,23 @@ class QueryTest {
         assertEquals(queryJsonHelper.variables, TestAuthenticationVariables(null, null, null))
     }
 
+    @Serializable
     data class QueryTestHelper(
         val query: String,
         val variables: TestAuthenticationVariables
     )
 
-    data class TestAuthenticationVariables(val user: String?, val password: String?, val deviceFormat: DeviceFormat?) {
+    @Serializable
+    data class TestAuthenticationVariables(
+        val user: String?,
+        val password: String?,
+        val deviceFormat: DeviceFormat?,
+        val deviceName: String? = android.os.Build.MODEL,
+        val deviceVersion: String? = android.os.Build.VERSION.RELEASE,
+        val appVersion: String = "VERSION_NAME",
+        val deviceType: DeviceType = DeviceType.android,
+        val deviceOS: String? = "os.version"
+    ) {
         fun equalsAuth(authenticationVariables: AuthenticationVariables): Boolean {
             return this.user == authenticationVariables.user &&
                     this.password == authenticationVariables.password
