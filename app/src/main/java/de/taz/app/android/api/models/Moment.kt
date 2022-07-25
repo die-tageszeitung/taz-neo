17c0ad711d23d631fa6cd21e179b1dd501c0d0cd
Felix Höffken <felix@ctrl.alt.coop>
2022-07-25 18:11:49 +0200

    Use lowercase instead of toLowercase

--- app/src/main/java/de/taz/app/android/api/models/Moment.kt
+++ app/src/main/java/de/taz/app/android/api/models/Moment.kt
@@ -106,7 +106,7 @@ data class Moment(
 
     fun getIndexHtmlForAnimated(): FileEntry? {
         return momentList.firstOrNull {
-            it.name.toLowerCase(Locale.ENGLISH).endsWith("index.html")
+            it.name.lowercase(Locale.ENGLISH).endsWith("index.html")
         }
     }
 }
