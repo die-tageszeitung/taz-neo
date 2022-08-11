52205345ed3f2dd40109a2661c48ac58f8886088
eike <eike@ctrl.alt.coop>
2022-08-11 18:00:01 +0200

    Add migration to remove Article.bookmarked

--- app/src/main/java/de/taz/app/android/persistence/AppDatabase.kt
+++ app/src/main/java/de/taz/app/android/persistence/AppDatabase.kt
@@ -12,7 +12,7 @@ import de.taz.app.android.persistence.join.*
 import de.taz.app.android.persistence.typeconverters.*
 import de.taz.app.android.util.SingletonHolder
 
-const val DATABASE_VERSION = 23
+const val DATABASE_VERSION = 24
 const val DATABASE_NAME = "db"
 
 val allMigrations = arrayOf(
@@ -38,6 +38,7 @@ val allMigrations = arrayOf(
     Migration20to21,
     Migration21to22,
     Migration22to23,
+    Migration23to24,
 )
 
 @Database(
