c80083a9c9448d592f5ad2ef649d987b785987ba
Felix Höffken <felix@ctrl.alt.coop>
2022-08-17 14:59:52 +0200

    Add functions to get applicationScope

--- app/src/main/java/de/taz/app/android/monkey/ApplicationScope.kt
+++ app/src/main/java/de/taz/app/android/monkey/ApplicationScope.kt
@@ -1,3 +1,12 @@
 package de.taz.app.android.monkey
 
-val Fragment.applicationScope
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import de.taz.app.android.TazApplication
+import kotlinx.coroutines.CoroutineScope
+
+fun Fragment.getApplicationScope(): CoroutineScope =
+    (requireActivity().application as TazApplication).applicationScope
+
+fun AppCompatActivity.getApplicationScope(): CoroutineScope =
+    (application as TazApplication).applicationScope
