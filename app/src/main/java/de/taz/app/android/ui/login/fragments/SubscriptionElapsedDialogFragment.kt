849cd5f61e2a269cfc8f7a57bd36a73e5379d8c9
Felix Höffken <felix@ctrl.alt.coop>
2022-08-18 11:32:45 +0200

    Make BottomSheet scrollable

--- app/src/main/java/de/taz/app/android/ui/login/fragments/SubscriptionElapsedDialogFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/login/fragments/SubscriptionElapsedDialogFragment.kt
@@ -3,21 +3,20 @@ package de.taz.app.android.ui.login.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import de.taz.app.android.R
 import de.taz.app.android.base.ViewBindingBottomSheetFragment
 import de.taz.app.android.databinding.FragmentSubscriptionElapsedDialogBinding
 
 
-class SubscriptionElapsedDialogFragment : ViewBindingBottomSheetFragment<FragmentSubscriptionElapsedDialogBinding>() {
+class SubscriptionElapsedDialogFragment :
+    ViewBindingBottomSheetFragment<FragmentSubscriptionElapsedDialogBinding>() {
 
     private val viewModel by viewModels<SubscriptionElapsedDialogFragmentViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.elapsedString.observe(this) {
-            viewBinding.title.text =
-                getString(R.string.popup_login_elapsed_header, it)
+            viewBinding.title.text = getString(R.string.popup_login_elapsed_header, it)
         }
 
         viewBinding.sendButton.setOnClickListener {
@@ -25,13 +24,6 @@ class SubscriptionElapsedDialogFragment : ViewBindingBottomSheetFragment<Fragmen
                 viewBinding.messageToSubscriptionService.text.toString(),
                 viewBinding.letTheSubscriptionServiceContactYouCheckbox.isChecked
             )
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        (dialog as BottomSheetDialog).behavior.apply {
-            isFitToContents = false
         }
     }
 }
