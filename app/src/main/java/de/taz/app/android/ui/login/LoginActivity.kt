ddb524443fd968d090afcecb783ca5dcbeb97d6a
Johannes Dillmann <johannes@ctrl.alt.coop>
2022-08-31 17:14:15 +0200

    [162] Remove "hack" to prevent tinting the bottom nav icons

--- app/src/main/java/de/taz/app/android/ui/login/LoginActivity.kt
+++ app/src/main/java/de/taz/app/android/ui/login/LoginActivity.kt
@@ -70,8 +70,6 @@ class LoginActivity : ViewBindingActivity<ActivityLoginBinding>() {
         article = intent.getStringExtra(LOGIN_EXTRA_ARTICLE)
 
         viewBinding.navigationBottom.apply {
-            itemIconTintList = null
-
             // hack to not auto select first item
             menu.getItem(0).isCheckable = false
 
