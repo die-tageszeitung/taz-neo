9a62393c0d33172ee2c17cca1cdecd2240dbd692
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2022-09-06 10:36:07 +0200

    Mary Poppins: Rename element to use correct grammar

--- app/src/main/java/de/taz/app/android/ui/login/fragments/LoginFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/login/fragments/LoginFragment.kt
@@ -67,7 +67,7 @@ class LoginFragment : LoginBaseFragment<FragmentLoginBinding>() {
             viewModel.requestPasswordReset()
         }
 
-        viewBinding.fragmentLoginForgotHelp.setOnClickListener {
+        viewBinding.fragmentLoginForgottenHelp.setOnClickListener {
             showHelpDialog(R.string.fragment_login_help)
         }
 
