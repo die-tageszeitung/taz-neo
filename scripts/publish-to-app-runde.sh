ff3291c637d5d232af95b1e17220b09d28870e47
Johannes Dillmann <johannes@ctrl.alt.coop>
2023-05-23 16:24:35 +0200

    [#15447] Nonfree Variante für manuelle Update App auf dl.taz.de verwenden

--- scripts/publish-to-app-runde.sh
+++ scripts/publish-to-app-runde.sh
@@ -12,9 +12,9 @@ AAPT_BIN="$BUILD_TOOLS_ROOT/aapt"
 RECENT_TAG=$(git describe --abbrev=0 --exclude="lmd-*")
 RELEASE_MESSAGE=$(git tag -l --format='%(contents)' "$RECENT_TAG")
 APP_VERSION=$CI_COMMIT_TAG
-APP_VERSION_CODE=$($AAPT_BIN dump badging app/build/outputs/apk/freeTazProduction/manualUpdateRelease/app-free-taz-production-manualUpdateRelease.apk | grep "package:" | sed -e "s/.*versionCode='//" -e "s/' .*//")
+APP_VERSION_CODE=$($AAPT_BIN dump badging app/build/outputs/apk/nonfreeTazProduction/manualUpdateRelease/app-nonfree-taz-production-manualUpdateRelease.apk | grep "package:" | sed -e "s/.*versionCode='//" -e "s/' .*//")
 
-FREE_APP_URL=$(curl --fail --header 'Accept: application/json' --header 'OCS-APIRequest: true' --header 'Content-Type: application/json' --data "{\"path\":\"${FREE_MANUAL_UPDATE_RELEASE_APK_PATH}\",\"shareType\":\"3\"}" https://cloud.alt.coop/ocs/v2.php/apps/files_sharing/api/v1/shares -u "$NEXTCLOUD_RELEASE_USER:$NEXTCLOUD_RELEASE_PASSWORD" | jq -r .ocs.data.url)
+MANUAL_UPDATE_APP_URL=$(curl --fail --header 'Accept: application/json' --header 'OCS-APIRequest: true' --header 'Content-Type: application/json' --data "{\"path\":\"${NONFREE_MANUAL_UPDATE_RELEASE_APK_PATH}\",\"shareType\":\"3\"}" https://cloud.alt.coop/ocs/v2.php/apps/files_sharing/api/v1/shares -u "$NEXTCLOUD_RELEASE_USER:$NEXTCLOUD_RELEASE_PASSWORD" | jq -r .ocs.data.url)
 DEBUG_APP_URL=$(curl --fail --header 'Accept: application/json' --header 'OCS-APIRequest: true' --header 'Content-Type: application/json' --data "{\"path\":\"${FREE_DEBUG_APK_PATH}\",\"shareType\":\"3\"}" https://cloud.alt.coop/ocs/v2.php/apps/files_sharing/api/v1/shares -u "$NEXTCLOUD_RELEASE_USER:$NEXTCLOUD_RELEASE_PASSWORD" | jq -r .ocs.data.url)
 STAGING_DEBUG_APP_URL=$(curl --fail --header 'Accept: application/json' --header 'OCS-APIRequest: true' --header 'Content-Type: application/json' --data "{\"path\":\"${FREE_STAGING_DEBUG_APK_PATH}\",\"shareType\":\"3\"}" https://cloud.alt.coop/ocs/v2.php/apps/files_sharing/api/v1/shares -u "$NEXTCLOUD_RELEASE_USER:$NEXTCLOUD_RELEASE_PASSWORD" | jq -r .ocs.data.url)
 
@@ -22,14 +22,14 @@ TAZ_RELEASE_JSON=$(
   jq -n '{
     staging_debug_link: $staging_debug_link,
     debug_link: $debug_link,
-    free_link: $free_link,
+    manual_update_link: $manual_update_link,
     version_code: $version_code,
     version_name: $version_name,
     release_note: $release_note
   }' \
     --arg staging_debug_link "$STAGING_DEBUG_APP_URL" \
     --arg debug_link "$DEBUG_APP_URL" \
-    --arg free_link "$FREE_APP_URL" \
+    --arg manual_update_link "$MANUAL_UPDATE_APP_URL" \
     --arg version_code "$APP_VERSION_CODE" \
     --arg version_name "$APP_VERSION" \
     --arg release_note "$RELEASE_MESSAGE"
