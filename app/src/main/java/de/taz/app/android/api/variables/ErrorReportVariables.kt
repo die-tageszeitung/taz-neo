6e49d6a4217c95933aab90d90b7516f7500f611e
eike <eike@ctrl.alt.coop>
2024-06-26 09:38:55 +0200

    [#17010] Add (taz) or (LMd) to appVersion in ErrorReports

--- app/src/main/java/de/taz/app/android/api/variables/ErrorReportVariables.kt
+++ app/src/main/java/de/taz/app/android/api/variables/ErrorReportVariables.kt
@@ -17,12 +17,18 @@ data class ErrorReportVariables(
     val storageType: String? = null,
     val errorProtocol: String? = null,
     val deviceOS: String? = System.getProperty("os.version"),
-    val appVersion: String = BuildConfig.VERSION_NAME,
+    val appVersion: String = "${BuildConfig.VERSION_NAME} ${
+        if (BuildConfig.IS_LMD) {
+            "(LMd)"
+        } else {
+            "(taz)"
+        }
+    }",
     val storageAvailable: String? = "${StatFs(Environment.getDataDirectory().path).availableBytes} Bytes",
     val storageUsed: String? = "${StatFs(Environment.getDataDirectory().path).totalBytes - StatFs(Environment.getDataDirectory().path).availableBytes} Bytes",
     val ramAvailable: String?,
     val ramUsed: String?,
-    val architecture: String? = android.os.Build.SUPPORTED_ABIS.joinToString (", "),
+    val architecture: String? = android.os.Build.SUPPORTED_ABIS.joinToString(", "),
     val deviceType: DeviceType = DeviceType.android,
     val deviceName: String = android.os.Build.MODEL,
     val deviceVersion: String? = "${android.os.Build.VERSION.SDK_INT} (Android ${android.os.Build.VERSION.RELEASE})",
