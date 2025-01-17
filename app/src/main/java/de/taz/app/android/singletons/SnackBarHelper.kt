a1980314714372aa319a93006edcf3f5e2a2966c
eike <eike@ctrl.alt.coop>
2025-01-17 13:09:36 +0100

    [Bug #17637] Action on whole snack bar

--- app/src/main/java/de/taz/app/android/singletons/SnackBarHelper.kt
+++ app/src/main/java/de/taz/app/android/singletons/SnackBarHelper.kt
@@ -3,33 +3,41 @@ package de.taz.app.android.singletons
 import android.content.Context
 import android.content.Intent
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.snackbar.Snackbar
 import de.taz.app.android.R
 import de.taz.app.android.audioPlayer.AudioPlayerService
 import de.taz.app.android.ui.bookmarks.BookmarkListActivity
+import de.taz.app.android.ui.bookmarks.BookmarkListItem
 
 /**
  * Singleton to simplify the creation of snackBars
  */
 object SnackBarHelper {
 
-    fun showSnack(
+    private fun showSnack(
         context: Context,
         view: View,
         anchor: View?,
         text: String,
         textAction: String,
+        textActionColor: Int? = null,
         action: (context: Context) -> Unit?,
     ) {
         val snack = Snackbar.make(
             context,
             view,
             text,
-            Snackbar.LENGTH_LONG
+            Snackbar.LENGTH_LONG,
         )
         snack.apply {
             anchorView = anchor
+            textActionColor?.let { setActionTextColor(it) }
             setAction(textAction) {
+                action(context)
+            }
+            // Additionally set the action on the whole snack bar:
+            snack.view.setOnClickListener{
                 action(context)
             }
             show()
@@ -45,7 +53,8 @@ object SnackBarHelper {
         view,
         anchor,
         context.getString(R.string.snack_article_bookmarked),
-        context.getString(R.string.snack_article_bookmark_action)
+        context.getString(R.string.snack_article_bookmark_action),
+        null,
     ) { goToBookmarks(context) }
 
     fun showDebookmarkSnack(
@@ -57,7 +66,8 @@ object SnackBarHelper {
         view,
         anchor,
         context.getString(R.string.snack_article_debookmarked),
-        context.getString(R.string.snack_article_bookmark_action)
+        context.getString(R.string.snack_article_bookmark_action),
+        null,
     ) { goToBookmarks(context) }
 
     fun showPlayListSnack(
@@ -69,8 +79,28 @@ object SnackBarHelper {
         view,
         anchor,
         context.getString(R.string.audioplayer_snackbar_added_to_playlist),
-        context.getString(R.string.audioplayer_show_in_playlist)
+        context.getString(R.string.audioplayer_show_in_playlist),
+        null,
     ) { showPlaylist(context) }
+
+    fun showDebookmarkUndoSnack(
+        context: Context,
+        view: View,
+        article: BookmarkListItem.Item,
+        action: (article: BookmarkListItem.Item) -> Unit?,
+    ) = showSnack(
+        context, view,
+        null,
+        context.getString( R.string.fragment_bookmarks_deleted),
+        context.getString(R.string.fragment_bookmarks_undo),
+        ResourcesCompat.getColor(
+            view.context.resources,
+            R.color.fragment_bookmarks_delete_background,
+            null
+        ),
+    ) {
+        action(article)
+    }
 
     private fun goToBookmarks(context: Context) {
         Intent(context, BookmarkListActivity::class.java).apply {
