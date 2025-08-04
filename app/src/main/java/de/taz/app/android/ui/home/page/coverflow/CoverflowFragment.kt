29cf96a9b8dceef497eb9e3d1c4bc31ef398cc75
Felix Höffken <felix@ctrl.alt.coop>
2025-08-04 09:42:45 +0200

    Use flowWithLifecycle

--- app/src/main/java/de/taz/app/android/ui/home/page/coverflow/CoverflowFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/home/page/coverflow/CoverflowFragment.kt
@@ -12,6 +12,7 @@ import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewModelScope
@@ -87,18 +88,20 @@ class CoverflowFragment : IssueFeedFragment<FragmentCoverflowBinding>() {
     private fun observePdfMode() {
         val tracker = Tracker.getInstance(requireContext().applicationContext)
 
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                // redraw pdfMode if changes after initial draw
-                viewModel.pdfMode.drop(1).onEach {
-                    viewBinding.fragmentCoverFlowGrid.adapter?.notifyDataSetChanged()
-                }.launchIn(lifecycleScope)
-                // once when initiated and whenever pdfMode changes track CoverFlow
-                viewModel.pdfMode.onEach { pdfMode ->
-                    tracker.trackCoverflowScreen(pdfMode)
-                }.launchIn(lifecycleScope)
-            }
-        }
+        // redraw pdfMode if changes after initial draw
+        viewModel.pdfMode
+            .flowWithLifecycle(lifecycle)
+            .drop(1)
+            .onEach {
+                viewBinding.fragmentCoverFlowGrid.adapter?.notifyDataSetChanged()
+            }.launchIn(lifecycleScope)
+
+        // once when initiated and whenever pdfMode changes track CoverFlow
+        viewModel.pdfMode
+            .flowWithLifecycle(lifecycle)
+            .onEach { pdfMode ->
+                tracker.trackCoverflowScreen(pdfMode)
+            }.launchIn(lifecycleScope)
     }
 
     /**
