465592029f05036f4b0407fbada267556764b243
eike <eike@ctrl.alt.coop>
2025-10-02 09:42:58 +0200

    Fix/designcorrections

--- app/src/main/java/de/taz/app/android/ui/home/HomeFragment.kt
+++ app/src/main/java/de/taz/app/android/ui/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.activityViewModels
