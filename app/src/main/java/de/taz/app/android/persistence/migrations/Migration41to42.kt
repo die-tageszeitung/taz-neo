8bdaf66c4ac90c5944af76c056f8c7499f190535
eike <eike@ctrl.alt.coop>
2026-08-12 09:56:45 +0200

    Fix NoSuchElementException

--- app/src/main/java/de/taz/app/android/persistence/migrations/Migration41to42.kt
+++ app/src/main/java/de/taz/app/android/persistence/migrations/Migration41to42.kt
@@ -6,7 +6,23 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 class Migration41to42 : Migration(41, 42) {
     override fun migrate(db: SupportSQLiteDatabase) {
         db.apply {
-            execSQL("ALTER TABLE `Page` DROP COLUMN `dateDownload`;")
+            // SQLite version in Android < 14 does not support 'ALTER TABLE DROP COLUMN'.
+            // We must use the 'temporary table' strategy instead.
+
+            // 1. Create a new table with the desired schema
+            execSQL("CREATE TABLE IF NOT EXISTS `Page_new` (`pdfFileName` TEXT NOT NULL, `title` TEXT, `pagina` TEXT, `type` TEXT, `frameList` TEXT, `baseUrl` TEXT NOT NULL, `podcastFileName` TEXT, `adIdList` TEXT, PRIMARY KEY(`pdfFileName`), FOREIGN KEY(`podcastFileName`) REFERENCES `Audio`(`fileName`) ON UPDATE NO ACTION ON DELETE NO ACTION )")
+
+            // 2. Copy the data from the old table to the new one
+            execSQL("INSERT INTO `Page_new` (`pdfFileName`, `title`, `pagina`, `type`, `frameList`, `baseUrl`, `podcastFileName`, `adIdList`) SELECT `pdfFileName`, `title`, `pagina`, `type`, `frameList`, `baseUrl`, `podcastFileName`, `adIdList` FROM `Page`")
+
+            // 3. Drop the old table
+            execSQL("DROP TABLE `Page`")
+
+            // 4. Rename the new table to the original name
+            execSQL("ALTER TABLE `Page_new` RENAME TO `Page`")
+
+            // 5. Re-create indices
+            execSQL("CREATE INDEX IF NOT EXISTS `index_Page_podcastFileName` ON `Page` (`podcastFileName`)")
         }
     }
 }
